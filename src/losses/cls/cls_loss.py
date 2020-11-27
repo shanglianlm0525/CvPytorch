d3c5c0ba86d61eabcf399ff21a43f6938fcb9b07
mingo <569793357@qq.com>
2020-11-27 16:26:33 +0800

    add LabelSmoothingCrossEntropy

---
+++ src/losses/cls/cls_loss.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2020/11/27 16:01
+# @Author : liumin
+# @File : cls_loss.py
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LabelSmoothingCrossEntropy(nn.Module):
+    def __init__(self, eps=0.1, reduction='mean'):
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        self.eps = eps
+        self.reduction = reduction
+
+    def forward(self, output, target):
+        c = output.size()[-1]
+        log_preds = F.log_softmax(output, dim=-1)
+        if self.reduction=='sum':
+            loss = -log_preds.sum()
+        else:
+            loss = -log_preds.sum(dim=-1)
+            if self.reduction=='mean':
+                loss = loss.mean()
+        return loss*self.eps/c + (1-self.eps) * F.nll_loss(log_preds, target, reduction=self.reduction)
