2a0e3ac87780de9d1179d4649acd53cca65d33d2
mingo <569793357@qq.com>
2020-11-30 15:34:53 +0800

    add adapter design patterns to MetricLogger

--- src/utils/calculate_weights.py
+++ src/utils/calculate_weights.py
@@ -5,14 +5,21 @@
 # @File : calculate_weights.py
 
 import os
+
+import torch
 from tqdm import tqdm
 import numpy as np
 
+
+'''
+    compute segmentation classes weights
+    https://github.com/jfzhang95/pytorch-deeplab-xception/blob/master/utils/calculate_weights.py
+'''
 def calculate_weigths_labels(dataset, dataloader, num_classes):
     # Create an instance from the data loader
     z = np.zeros((num_classes,))
     for sample in dataloader:
-        y = sample['label']
+        y = sample['target']
         y = y.detach().cpu().numpy()
         mask = (y >= 0) & (y < num_classes)
         labels = y[mask].astype(np.uint8)
@@ -25,3 +32,26 @@ def calculate_weigths_labels(dataset, dataloader, num_classes):
         class_weights.append(class_weight)
     ret = np.array(class_weights)
     return ret
+
+
+'''
+    compute detection classes weights
+    https://github.com/ultralytics/yolov5/blob/master/utils/general.py
+'''
+def labels_to_class_weights(labels, nc=80):
+    # Get class weights (inverse frequency) from training labels
+    if labels[0] is None:  # no labels loaded
+        return torch.Tensor()
+
+    labels = np.concatenate(labels, 0)  # labels.shape = (866643, 5) for COCO
+    classes = labels[:, 0].astype(np.int)  # labels = [class xywh]
+    weights = np.bincount(classes, minlength=nc)  # occurences per class
+
+    # Prepend gridpoint count (for uCE trianing)
+    # gpi = ((320 / 32 * np.array([1, 2, 4])) ** 2 * 3).sum()  # gridpoints per image
+    # weights = np.hstack([gpi * len(labels)  - weights.sum() * 9, weights * 9]) ** 0.5  # prepend gridpoints to start
+
+    weights[weights == 0] = 1  # replace empty bins with 1
+    weights = 1 / weights  # number of targets per class
+    weights /= weights.sum()  # normalize
+    return torch.from_numpy(weights)
---
+++ src/utils/tta.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2020/11/30 13:39
+# @Author : liumin
+# @File : tta.py
+
+'Test Time Augmentation (TTA)'
