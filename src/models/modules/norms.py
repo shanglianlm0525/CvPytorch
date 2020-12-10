a0a3a2d0d3eb966848285f10b1edc4688a1ac84e
mingo <569793357@qq.com>
2020-12-10 18:48:19 +0800

    divide GroupNorm from others

--- src/models/modules/norms.py
+++ src/models/modules/norms.py
@@ -11,10 +11,13 @@ import torch.nn as nn
 __avalible__ = {
     'BatchNorm1d': nn.BatchNorm1d, 'BatchNorm2d': nn.BatchNorm2d,'BatchNorm3d': nn.BatchNorm3d
     ,'InstanceNorm1d': nn.InstanceNorm1d, 'InstanceNorm2d': nn.InstanceNorm2d, 'InstanceNorm3d': nn.InstanceNorm3d
-    ,'GroupNorm': nn.GroupNorm, 'SyncBatchNorm': nn.SyncBatchNorm, 'LayerNorm': nn.LayerNorm
+    ,'GroupNorm': nn.GroupNorm, 'SyncBatchNorm': nn.SyncBatchNorm
 }
 
 
-def norm_layers(name,**params):
+def norm_layers(name,num_features):
     assert name in __avalible__.keys()
-    return __avalible__[name](**params)
+    if name == 'GroupNorm':
+        return __avalible__[name](num_groups=1,num_features=num_features)
+    else:
+        return __avalible__[name](num_features=num_features)
