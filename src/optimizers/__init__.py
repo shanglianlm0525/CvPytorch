98ae63fd0ed649b7115a671cccbe556458f56af6
mingo <569793357@qq.com>
2020-12-17 13:53:16 +0800

    move opitmizer to __init__

--- src/optimizers/__init__.py
+++ src/optimizers/__init__.py
@@ -1,13 +1,16 @@
 # !/usr/bin/env python
 # -- coding: utf-8 --
-# @Time : 2020/6/12 13:01
+# @Time : 2020/12/16 15:38
 # @Author : liumin
-# @File : optimizer.py
-from copy import deepcopy
-import torch
-import torch.optim as optim
+# @File : __init__.py
 
-def parser_optimizer(cfg, model):
+from copy import deepcopy
+from torch.optim import SGD, Adam, RMSprop
+
+__all__ = ['SGD','Adam','RMSprop']
+
+
+def build_optimizer(cfg, model):
     # params = [p for p in model.parameters() if p.requires_grad]
     _params = []
     # filter(lambda p: p.requires_grad, model.parameters())
@@ -20,11 +23,14 @@ def parser_optimizer(cfg, model):
                 _params[-1]["lr"] *= cfg.OPTIMIZER.BIAS_LR_MULTIPLIER or 1.0
 
     if cfg.OPTIMIZER.TYPE == "SGD":
-        optimizer = optim.SGD(_params)
+        '''torch.optim.SGD(params, lr=0.001, momentum=0, dampening=0, weight_decay=0, nesterov=False)'''
+        optimizer = SGD(_params)
     elif cfg.OPTIMIZER.TYPE == "Adam":
-        optimizer = optim.Adam(_params)
+        '''torch.optim.Adam(params, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, amsgrad=False)'''
+        optimizer = Adam(_params)
     elif cfg.OPTIMIZER.TYPE == 'RMSprop':
-        optimizer = optim.RMSprop(_params)
+        '''torch.optim.RMSprop(params, lr=0.01, alpha=0.99, eps=1e-08, weight_decay=0, momentum=0, centered=False)'''
+        optimizer = RMSprop(_params)
     else:
         raise ValueError("Unsupported optimizer type: {}".format(cfg.OPTIMIZER.TYPE))
 
