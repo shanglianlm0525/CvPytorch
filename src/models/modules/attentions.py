51282ddcbb270a502a521f8e93cb1b7c0fe5e23f
mingo <569793357@qq.com>
2020-12-25 15:33:11 +0800

    add fca and modify conv to linear in CBAM and BAM

--- src/models/modules/attentions.py
+++ src/models/modules/attentions.py
@@ -221,15 +221,15 @@ class ChannelAttentionModule(nn.Module):
         self.max_pool = nn.AdaptiveMaxPool2d(1)
 
         self.shared_MLP = nn.Sequential(
-            nn.Conv2d(in_channels=channel, out_channels=mid_channel, kernel_size=1, bias=False),
+            nn.Linear(in_features=channel, out_features=mid_channel),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=mid_channel, out_channels=channel, kernel_size=1, bias=False),
+            nn.Linear(in_features=mid_channel, out_features=channel)
         )
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        avgout = self.shared_MLP(self.avg_pool(x))
-        maxout = self.shared_MLP(self.max_pool(x))
+        avgout = self.shared_MLP(self.avg_pool(x).view(x.size(0),-1)).unsqueeze(2).unsqueeze(3)
+        maxout = self.shared_MLP(self.max_pool(x).view(x.size(0),-1)).unsqueeze(2).unsqueeze(3)
         return self.sigmoid(avgout + maxout)
 
 
@@ -270,14 +270,14 @@ class ChannelAttention(nn.Module):
 
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.shared_MLP = nn.Sequential(
-            nn.Conv2d(in_channels=channel, out_channels=mid_channel, kernel_size=1, bias=False),
+            nn.Linear(in_features=channel, out_features=mid_channel),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=mid_channel, out_channels=channel, kernel_size=1, bias=False),
+            nn.Linear(in_features=mid_channel, out_features=channel)
         )
 
     def forward(self, x):
-        avg = self.avg_pool(x)
-        out = self.shared_MLP(avg).expand_as(x)
+        avg = self.avg_pool(x).view(x.size(0), -1)
+        out = self.shared_MLP(avg).unsqueeze(2).unsqueeze(3).expand_as(x)
         return out
 
 
@@ -328,6 +328,100 @@ class SplitAttention(nn.Module):
     def forward(self, x):
         pass
 
+'''
+class SplAtConv2d(Module):
+    """Split-Attention Conv2d
+    """
+    def __init__(self, in_channels, channels, kernel_size, stride=(1, 1), padding=(0, 0),
+                 dilation=(1, 1), groups=1, bias=True,
+                 radix=2, reduction_factor=4,
+                 rectify=False, rectify_avg=False, norm_layer=None,
+                 dropblock_prob=0.0, **kwargs):
+        super(SplAtConv2d, self).__init__()
+        padding = _pair(padding)
+        self.rectify = rectify and (padding[0] > 0 or padding[1] > 0)
+        self.rectify_avg = rectify_avg
+        inter_channels = max(in_channels*radix//reduction_factor, 32)
+        self.radix = radix
+        self.cardinality = groups
+        self.channels = channels
+        self.dropblock_prob = dropblock_prob
+        if self.rectify:
+            from rfconv import RFConv2d
+            self.conv = RFConv2d(in_channels, channels*radix, kernel_size, stride, padding, dilation,
+                                 groups=groups*radix, bias=bias, average_mode=rectify_avg, **kwargs)
+        else:
+            self.conv = Conv2d(in_channels, channels*radix, kernel_size, stride, padding, dilation,
+                               groups=groups*radix, bias=bias, **kwargs)
+        self.use_bn = norm_layer is not None
+        if self.use_bn:
+            self.bn0 = norm_layer(channels*radix)
+        self.relu = ReLU(inplace=True)
+        self.fc1 = Conv2d(channels, inter_channels, 1, groups=self.cardinality)
+        if self.use_bn:
+            self.bn1 = norm_layer(inter_channels)
+        self.fc2 = Conv2d(inter_channels, channels*radix, 1, groups=self.cardinality)
+        if dropblock_prob > 0.0:
+            self.dropblock = DropBlock2D(dropblock_prob, 3)
+        self.rsoftmax = rSoftMax(radix, groups)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.use_bn:
+            x = self.bn0(x)
+        if self.dropblock_prob > 0.0:
+            x = self.dropblock(x)
+        x = self.relu(x)
+
+        batch, rchannel = x.shape[:2]
+        if self.radix > 1:
+            if torch.__version__ < '1.5':
+                splited = torch.split(x, int(rchannel//self.radix), dim=1)
+            else:
+                splited = torch.split(x, rchannel//self.radix, dim=1)
+            gap = sum(splited)
+        else:
+            gap = x
+        gap = F.adaptive_avg_pool2d(gap, 1)
+        gap = self.fc1(gap)
+
+        if self.use_bn:
+            gap = self.bn1(gap)
+        gap = self.relu(gap)
+
+        atten = self.fc2(gap)
+        atten = self.rsoftmax(atten).view(batch, -1, 1, 1)
+
+        if self.radix > 1:
+            attens = torch.split(atten, rchannel//self.radix, dim=1)
+            out = sum([att*split for (att, split) in zip(attens, splited)])
+        else:
+            out = atten * x
+        return out.contiguous()
+'''
+
+class FCABlock(nn.Module):
+    """
+        FcaNet: Frequency Channel Attention Networks
+        https://arxiv.org/pdf/2012.11879.pdf
+    """
+    def __init__(self, channel, reduction=16, dct_weight=None):
+        super(FCABlock, self).__init__()
+        mid_channel = channel // reduction
+        self.dct_weight = dct_weight
+        self.excitation = nn.Sequential(
+            nn.Linear(channel, mid_channel, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(mid_channel, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = torch.sum(x*self.dct_weight, dim=[2,3])
+        z = self.excitation(y).view(b, c, 1, 1)
+        return x * z.expand_as(x)
+
 
 if __name__=='__main__':
     # model = SEBlock(16)
@@ -335,7 +429,8 @@ if __name__=='__main__':
     # model = NonLocalBlock(16)
     # model = GlobalContextBlock(inplanes=16, ratio=4, pooling_type='att')
     # model = CBAM(16)
-    model = BAM(16)
+    # model = BAM(16)
+    model = FCABlock(16)
     print(model)
 
     input = torch.randn(1, 16, 64, 64)
