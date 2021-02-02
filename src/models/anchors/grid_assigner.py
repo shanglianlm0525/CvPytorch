26ebf4daac46ecf20604cdb6eca0a09730d092f4
mingo <569793357@qq.com>
2021-02-02 10:40:43 +0800

    anchors

---
+++ src/models/anchors/grid_assigner.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2021/1/20 12:55
+# @Author : liumin
+# @File : grid_assigner.py
+
+import torch
+import torch.nn as nn
+
+
+class GridAssigner(nn.Module):
+    def __init__(self):
+        super(GridAssigner, self).__init__()
+
+
+    def forward(self, x):
+        pass
---
+++ src/models/anchors/yolo_anchor_generator.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2021/1/19 13:35
+# @Author : liumin
+# @File : yolo_anchor_generator.py
+
+import torch
+import torch.nn as nn
+
+
+class YOLOAnchorGenerator(nn.Modue):
+    def __init__(self):
+        super(YOLOAnchorGenerator, self).__init__()
+
+
+    def forward(self, feats):
+        pass
