c797b0059c9824201f7fbbb54a7bdf8297cc71f8
mingo <569793357@qq.com>
2021-02-04 14:37:26 +0800

    add focal, bce, ce, lovasz loss

--- src/losses/seg_loss.py
+++ src/losses/seg_loss.py
@@ -1,31 +1,31 @@
 # !/usr/bin/env python
 # -- coding: utf-8 --
-# @Time : 2020/7/27 10:14
+# @Time : 2021/2/3 9:35
 # @Author : liumin
 # @File : seg_loss.py
 
-import numpy as np
 import torch
-import torch.nn.functional as torchF
 import torch.nn as nn
-from .lovasz_losses import lovasz_softmax
+import torch.nn.functional as F
+import numpy as np
 
-def make_one_hot(labels, classes):
-    one_hot = torch.cuda.FloatTensor(labels.size()[0], classes, labels.size()[2], labels.size()[3]).zero_()
-    target = one_hot.scatter_(1, labels.data, 1)
+from CvPytorch.src.losses.lovasz_losses import lovasz_softmax
+
+
+def make_one_hot(labels, classes, ignore_index=None):
+    if ignore_index is not None:
+        ignore_i = labels == ignore_index
+        # preset the element to be ignored to 0
+        labels[ignore_i] = 0
+        one_hot = torch.cuda.FloatTensor(labels.size()[0], classes, labels.size()[2], labels.size()[3]).zero_()
+        target = one_hot.scatter_(1, labels.data, 1)
+        # remove ignore index
+        ignore_i = ignore_i.expand(ignore_i.shape[0], classes, ignore_i.shape[2], ignore_i.shape[3])
+        target[ignore_i] = 0
+    else:
+        one_hot = torch.cuda.FloatTensor(labels.size()[0], classes, labels.size()[2], labels.size()[3]).zero_()
+        target = one_hot.scatter_(1, labels.data, 1)
     return target
-
-
-def get_weights(target):
-    t_np = target.view(-1).data.cpu().numpy()
-
-    classes, counts = np.unique(t_np, return_counts=True)
-    cls_w = np.median(counts) / counts
-    # cls_w = class_weight.compute_class_weight('balanced', classes, t_np)
-
-    weights = np.ones(7)
-    weights[classes] = cls_w
-    return torch.from_numpy(weights).float().cuda()
 
 
 class CrossEntropyLoss2d(nn.Module):
@@ -34,19 +34,41 @@ class CrossEntropyLoss2d(nn.Module):
         self.CE = nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_index, reduction=reduction)
 
     def forward(self, output, target):
-        loss = self.CE(output, target)
+        assert output.shape[0] == target.shape[0] and output.shape[2:] == target.shape[2:]
+        target = target.squeeze(1).long()
+        return self.CE(output, target)
+
+
+class BCEWithLogitsLoss2d(nn.Module):
+    def __init__(self, weight=None, pos_weight=None, ignore_index=255, reduction='mean'):
+        super(BCEWithLogitsLoss2d, self).__init__()
+        self.ignore_index = ignore_index
+        self.weight = weight
+        self.pos_weight = pos_weight
+        self.reduction = reduction
+        # self.BCE = nn.BCEWithLogitsLoss(weight=weight, pos_weight=pos_weight, reduction=reduction)
+
+    def forward(self, output, target):
+        assert output.shape[0] == target.shape[0] and output.shape[2:] == target.shape[2:]
+        target = make_one_hot(target.long(), output.size()[1], self.ignore_index)
+        pos_weight = self.pos_weight.view(1, -1, 1, 1).expand(target.shape).cuda()
+        loss = F.binary_cross_entropy_with_logits(output, target.float(), weight=self.weight, pos_weight=pos_weight,
+                                                 reduction=self.reduction)
         return loss
 
 
 class DiceLoss(nn.Module):
-    def __init__(self, smooth=1.):
+    def __init__(self, smooth=1., ignore_index=255):
         super(DiceLoss, self).__init__()
+        self.ignore_index = ignore_index
         self.smooth = smooth
+        self.softmax = nn.Softmax(dim=1)
 
     def forward(self, output, target):
-        target = make_one_hot(target.unsqueeze(dim=1), classes=output.size()[1])
-        output = torchF.softmax(output)
-        # output = output.sigmoid()
+        assert output.shape[0] == target.shape[0] and output.shape[2:] == target.shape[2:]
+        target = make_one_hot(target.long(), output.size()[1], self.ignore_index)
+        output = self.softmax(output)
+        target = target.float()
         # have to be contiguous since they may from a torch.view op
         output_flat = output.contiguous().view(-1)
         target_flat = target.contiguous().view(-1)
@@ -56,148 +78,55 @@ class DiceLoss(nn.Module):
         return loss
 
 
-class BatchFocalLoss(nn.Module):
-    def __init__(self, gamma=2, alpha=None, ignore_index=255, size_average=True):
-        super(BatchFocalLoss, self).__init__()
-        self.size_average = size_average
-        self.CE_loss = nn.CrossEntropyLoss(reduce=False, ignore_index=ignore_index, weight=alpha)
+class FocalLoss(nn.Module):
+    def __init__(self, gamma=2, alpha=None, ignore_index=255, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        self.CE = CrossEntropyLoss2d(weight=alpha,ignore_index=ignore_index, reduction=reduction)
 
     def forward(self, output, target):
-        logpt = self.CE_loss(output, target)
+        logpt = self.CE(output, target)
         pt = torch.exp(-logpt)
-        loss = ((1 - pt) ** self.gamma) * logpt
-        if self.size_average:
-            return loss.mean()
-        return loss.sum()
-
-
-class CE_DiceLoss(nn.Module):
-    def __init__(self, smooth=1, reduction='mean', ignore_index=255, weight=None):
-        super(CE_DiceLoss, self).__init__()
-        self.smooth = smooth
-        self.dice = DiceLoss()
-        self.cross_entropy = nn.CrossEntropyLoss(weight=weight, reduction=reduction, ignore_index=ignore_index)
-
-    def forward(self, output, target):
-        CE_loss = self.cross_entropy(output, target)
-        dice_loss = self.dice(output, target)
-        return CE_loss + dice_loss
+        loss = ((1-pt)**self.gamma) * logpt
+        return loss
 
 
 class LovaszSoftmax(nn.Module):
-    def __init__(self, classes='present', per_image=False, ignore=255):
+    def __init__(self, classes='present', per_image=False, ignore_index=255):
         super(LovaszSoftmax, self).__init__()
         self.smooth = classes
         self.per_image = per_image
-        self.ignore_index = ignore
+        self.ignore_index = ignore_index
 
     def forward(self, output, target):
-        logits = torchF.softmax(output, dim=1)
+        logits = F.softmax(output, dim=1)
         loss = lovasz_softmax(logits, target, ignore=self.ignore_index)
         return loss
 
 
-class MixSoftmaxCrossEntropyLoss(nn.CrossEntropyLoss):
-    def __init__(self, aux=True, aux_weight=0.2, ignore_label=-1):
-        super(MixSoftmaxCrossEntropyLoss, self).__init__(ignore_index=ignore_label)
-        self.aux = aux
-        self.aux_weight = aux_weight
+class CE_DiceLoss(nn.Module):
+    def __init__(self, smooth=1, weight=None, ignore_index=255, reduction='mean'):
+        super(CE_DiceLoss, self).__init__()
+        self.smooth = smooth
+        self.dice = DiceLoss()
+        self.ce = CrossEntropyLoss2d(weight=weight, reduction=reduction, ignore_index=ignore_index)
 
-    def _aux_forward(self, *inputs):
-        *preds, target = tuple(inputs)
+    def forward(self, output, target):
+        ce_loss = self.ce(output, target)
+        dice_loss = self.dice(output, target)
+        return ce_loss + dice_loss
 
-        loss = super(MixSoftmaxCrossEntropyLoss, self).forward(preds[0], target)
-        for i in range(1, len(preds)):
-            aux_loss = super(MixSoftmaxCrossEntropyLoss, self).forward(preds[i], target)
-            loss += self.aux_weight * aux_loss
-        return loss
+if __name__ == '__main__':
+    ce_loss = CrossEntropyLoss2d()
+    input = torch.randn(3, 5)
+    target = torch.empty(3, dtype=torch.long).random_(5)
+    output = ce_loss(input, target)
+    print('ce_loss', output.item())
 
-    def forward(self, *inputs):
-        preds, target = tuple(inputs)
-        inputs = tuple(list(preds) + [target])
-        if self.aux:
-            return self._aux_forward(*inputs)
-        else:
-            return super(MixSoftmaxCrossEntropyLoss, self).forward(*inputs)
-
-
-class SoftmaxCrossEntropyOHEMLoss(nn.Module):
-    def __init__(self, ignore_label=-1, thresh=0.7, min_kept=256, use_weight=True):
-        super(SoftmaxCrossEntropyOHEMLoss, self).__init__()
-        self.ignore_label = ignore_label
-        self.thresh = float(thresh)
-        self.min_kept = int(min_kept)
-        if use_weight:
-            print("w/ class balance")
-            weight = torch.FloatTensor([0.8373, 0.918, 0.866, 1.0345, 1.0166, 0.9969, 0.9754,
-                                        1.0489, 0.8786, 1.0023, 0.9539, 0.9843, 1.1116, 0.9037, 1.0865, 1.0955,
-                                        1.0865, 1.1529, 1.0507])
-            self.criterion = torch.nn.CrossEntropyLoss(weight=weight, ignore_index=ignore_label)
-        else:
-            print("w/o class balance")
-            self.criterion = torch.nn.CrossEntropyLoss(ignore_index=ignore_label)
-
-    def forward(self, predict, target, weight=None):
-        assert not target.requires_grad
-        assert predict.dim() == 4
-        assert target.dim() == 3
-        assert predict.size(0) == target.size(0), "{0} vs {1} ".format(predict.size(0), target.size(0))
-        assert predict.size(2) == target.size(1), "{0} vs {1} ".format(predict.size(2), target.size(1))
-        assert predict.size(3) == target.size(2), "{0} vs {1} ".format(predict.size(3), target.size(3))
-
-        n, c, h, w = predict.size()
-        input_label = target.data.cpu().numpy().ravel().astype(np.int32)
-        x = np.rollaxis(predict.data.cpu().numpy(), 1).reshape((c, -1))
-        input_prob = np.exp(x - x.max(axis=0).reshape((1, -1)))
-        input_prob /= input_prob.sum(axis=0).reshape((1, -1))
-
-        valid_flag = input_label != self.ignore_label
-        valid_inds = np.where(valid_flag)[0]
-        label = input_label[valid_flag]
-        num_valid = valid_flag.sum()
-        if self.min_kept >= num_valid:
-            print('Labels: {}'.format(num_valid))
-        elif num_valid > 0:
-            prob = input_prob[:, valid_flag]
-            pred = prob[label, np.arange(len(label), dtype=np.int32)]
-            threshold = self.thresh
-            if self.min_kept > 0:
-                index = pred.argsort()
-                threshold_index = index[min(len(index), self.min_kept) - 1]
-                if pred[threshold_index] > self.thresh:
-                    threshold = pred[threshold_index]
-            kept_flag = pred <= threshold
-            valid_inds = valid_inds[kept_flag]
-
-        label = input_label[valid_inds].copy()
-        input_label.fill(self.ignore_label)
-        input_label[valid_inds] = label
-        valid_flag_new = input_label != self.ignore_label
-        # print(np.sum(valid_flag_new))
-        target = torch.from_numpy(input_label.reshape(target.size())).long().cuda()
-
-        return self.criterion(predict, target)
-
-
-class MixSoftmaxCrossEntropyOHEMLoss(SoftmaxCrossEntropyOHEMLoss):
-    def __init__(self, aux=False, aux_weight=0.2, ignore_index=-1):
-        super(MixSoftmaxCrossEntropyOHEMLoss, self).__init__(ignore_label=ignore_index)
-        self.aux = aux
-        self.aux_weight = aux_weight
-
-    def _aux_forward(self, *inputs):
-        *preds, target = tuple(inputs)
-
-        loss = super(MixSoftmaxCrossEntropyOHEMLoss, self).forward(preds[0], target)
-        for i in range(1, len(preds)):
-            aux_loss = super(MixSoftmaxCrossEntropyOHEMLoss, self).forward(preds[i], target)
-            loss += self.aux_weight * aux_loss
-        return loss
-
-    def forward(self, *inputs):
-        preds, target = tuple(inputs)
-        inputs = tuple(list(preds) + [target])
-        if self.aux:
-            return self._aux_forward(*inputs)
-        else:
-            return super(MixSoftmaxCrossEntropyOHEMLoss, self).forward(*inputs)
+    target = torch.ones([5, 6], dtype=torch.float32)  # 64 classes, batch size = 10
+    output = torch.full([5, 6], 1.5)  # A prediction (logit)
+    pos_weight = torch.ones([6])  # All weights are equal to 1
+    criterion = BCEWithLogitsLoss2d(pos_weight=pos_weight)
+    criterion(output, target)  # -log(sigmoid(1.5))
+    print('bce_loss', output)
