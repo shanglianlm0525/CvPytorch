44f525a32df5664e88a5e3d405ab5c90ba7f25ab
mingo <569793357@qq.com>
2021-03-16 12:56:06 +0800

    add pennfudan dataset

--- src/datasets/pennfudan.py
+++ src/datasets/pennfudan.py
@@ -1,36 +1,30 @@
 # !/usr/bin/env python
 # -- coding: utf-8 --
-# @Time : 2020/8/17 13:58
+# @Time : 2021/3/15 14:17
 # @Author : liumin
 # @File : pennfudan.py
 
 import os
-
-import cv2
 import torch
-from PIL import Image
 from glob2 import glob
 import numpy as np
-import random
 from torch.utils.data import Dataset
-from PIL import Image, ImageOps, ImageFilter
-from torchvision import transforms as transformsT
-import xml.etree.ElementTree as ET
-import torchvision.transforms.functional as torchF
-from pycocotools import mask as coco_mask
-from pycocotools.coco import COCO
+from PIL import Image
+from ..utils.coco_utils import convert_to_coco_api
 
-class PennFudanDetection(Dataset):
-    '''
+'''
     Penn-Fudan Database
     https://www.cis.upenn.edu/~jshi/ped_html/
-    '''
+'''
+
+class PennFudanDetection(Dataset):
+    ignore_index = 255
     def __init__(self, data_cfg, dictionary=None, transform=None, target_transform=None, stage='train'):
         super(PennFudanDetection, self).__init__()
         self.data_cfg = data_cfg
         self.dictionary = dictionary
-        self.transform = SSDAugmentation()
-        self.target_transform = VOCAnnotationTransform()
+        self.transform = transform # build_transforms(data_cfg.TRANSFORMS)
+        self.target_transform = target_transform
         self.stage = stage
 
         self.num_classes = len(self.dictionary)
@@ -38,8 +32,8 @@ class PennFudanDetection(Dataset):
         self.name2id = dict(zip(self.category, range(self.num_classes)))
         self.id2name = {v: k for k, v in self.name2id.items()}
 
-        self._imgs = list()
-        self._labels = list()
+        self._imgs = []
+        self._targets = []
         if self.stage == 'infer':
             if data_cfg.INDICES is not None:
                 with open(data_cfg.INDICES, 'r') as fd:
@@ -53,195 +47,38 @@ class PennFudanDetection(Dataset):
                 raise RuntimeError(
                     "Found 0 images in subfolders of: " + data_cfg.IMG_DIR if data_cfg.INDICES is not None else data_cfg.INDICES + "\n")
         else:
-            for line in open(data_cfg.INDICES):
-                imgpath,labelpath = line.strip().split(' ')
-                self._imgs.append(os.path.join(data_cfg.IMG_DIR, imgpath))
-                self._labels.append(os.path.join(data_cfg.LABELS.DET_DIR, labelpath))
-            assert len(self._imgs) == len(self._labels), 'len(self._imgs) should be equals to len(self._labels)'
-
-    def __getitem__(self, idx):
-        img = cv2.imread(self._imgs[idx])
-        target = ET.parse(self._labels[idx]).getroot()
-
-        height, width, channels = img.shape
-
-        if self.target_transform is not None:
-            target = self.target_transform(target, width, height)
-
-        if self.transform is not None:
-            target = np.array(target)
-            img, boxes, labels = self.transform(img, target[:, :4], target[:, 4])
-            # to rgb
-            img = img[:, :, (2, 1, 0)]
-            # img = img.transpose(2, 0, 1)
-            target = np.hstack((boxes, np.expand_dims(labels, axis=1)))
-
-        return torch.from_numpy(img).permute(2, 0, 1), target
-
-    def __len__(self):
-        return len(self._imgs)
-
-def convert_coco_poly_to_mask(segmentations, height, width):
-    masks = []
-    for polygons in segmentations:
-        rles = coco_mask.frPyObjects(polygons, height, width)
-        mask = coco_mask.decode(rles)
-        if len(mask.shape) < 3:
-            mask = mask[..., None]
-        mask = torch.as_tensor(mask, dtype=torch.uint8)
-        mask = mask.any(dim=2)
-        masks.append(mask)
-    if masks:
-        masks = torch.stack(masks, dim=0)
-    else:
-        masks = torch.zeros((0, height, width), dtype=torch.uint8)
-    return masks
-
-class ConvertCocoPolysToMask(object):
-    def __call__(self, image, target):
-        w, h = image.size
-
-        image_id = target["image_id"]
-        image_id = torch.tensor([image_id])
-
-        anno = target["annotations"]
-
-        anno = [obj for obj in anno if obj['iscrowd'] == 0]
-
-        boxes = [obj["bbox"] for obj in anno]
-        # guard against no boxes via resizing
-        boxes = torch.as_tensor(boxes, dtype=torch.float32).reshape(-1, 4)
-        boxes[:, 2:] += boxes[:, :2]
-        boxes[:, 0::2].clamp_(min=0, max=w)
-        boxes[:, 1::2].clamp_(min=0, max=h)
-
-        classes = [obj["category_id"] for obj in anno]
-        classes = torch.tensor(classes, dtype=torch.int64)
-
-        segmentations = [obj["segmentation"] for obj in anno]
-        masks = convert_coco_poly_to_mask(segmentations, h, w)
-
-        keypoints = None
-        if anno and "keypoints" in anno[0]:
-            keypoints = [obj["keypoints"] for obj in anno]
-            keypoints = torch.as_tensor(keypoints, dtype=torch.float32)
-            num_keypoints = keypoints.shape[0]
-            if num_keypoints:
-                keypoints = keypoints.view(num_keypoints, -1, 3)
-
-        keep = (boxes[:, 3] > boxes[:, 1]) & (boxes[:, 2] > boxes[:, 0])
-        boxes = boxes[keep]
-        classes = classes[keep]
-        masks = masks[keep]
-        if keypoints is not None:
-            keypoints = keypoints[keep]
-
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = classes
-        target["masks"] = masks
-        target["image_id"] = image_id
-        if keypoints is not None:
-            target["keypoints"] = keypoints
-
-        # for conversion to coco api
-        area = torch.tensor([obj["area"] for obj in anno])
-        iscrowd = torch.tensor([obj["iscrowd"] for obj in anno])
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-
-        return image, target
-
-class Compose(object):
-    def __init__(self, transforms):
-        self.transforms = transforms
-
-    def __call__(self, image, target):
-        for t in self.transforms:
-            image, target = t(image, target)
-        return image, target
-
-
-class RandomHorizontalFlip(object):
-    def __init__(self, prob):
-        self.prob = prob
-
-    def __call__(self, image, target):
-        if random.random() < self.prob:
-            height, width = image.shape[-2:]
-            image = image.flip(-1)
-            bbox = target["boxes"]
-            bbox[:, [0, 2]] = width - bbox[:, [2, 0]]
-            target["boxes"] = bbox
-            if "masks" in target:
-                target["masks"] = target["masks"].flip(-1)
-        return image, target
-
-
-class ToTensor(object):
-    def __call__(self, image, target):
-        image = torchF.to_tensor(image)
-        return image, target
-
-def get_transform(train):
-    transforms = []
-    transforms.append(ToTensor())
-    if train:
-        transforms.append(RandomHorizontalFlip(0.5))
-    return Compose(transforms)
-
-class PennFudanSegmentation(Dataset):
-    def __init__(self, data_cfg, dictionary=None, transform=None, target_transform=None, stage='train'):
-        '''
-        Penn-Fudan Database
-        https://www.cis.upenn.edu/~jshi/ped_html/
-        '''
-        super(PennFudanSegmentation, self).__init__()
-        self.data_cfg = data_cfg
-        self.dictionary = dictionary
-        self.transform = get_transform(train=stage=='train')
-        self.target_transform = target_transform
-        self.stage = stage
-
-        self.num_classes = len(self.dictionary) + 1
-
-        self._imgs = list()
-        self._labels = list()
-        if self.stage == 'infer':
             if data_cfg.INDICES is not None:
-                with open(data_cfg.INDICES, 'r') as fd:
-                    self._imgs.extend([os.path.join(data_cfg.IMG_DIR, line.strip()) for line in fd])
+                for line in open(data_cfg.INDICES):
+                    imgpath, labelpath = line.strip().split(' ')
+                    self._imgs.append(os.path.join(data_cfg.IMG_DIR, imgpath))
+                    self._targets.append(os.path.join(data_cfg.LABELS.SEG_DIR, labelpath))
             else:
-                for root, fnames, _ in sorted(os.walk(data_cfg.IMG_DIR)):
-                    for fname in sorted(fnames):
-                        self._imgs.extend(glob(os.path.join(root, fname, data_cfg.IMG_SUFFIX)))
+                self._imgs = glob(os.path.join(data_cfg.IMG_DIR, data_cfg.IMG_SUFFIX))
+                self._targets = glob(os.path.join(data_cfg.LABELS.SEG_DIR, data_cfg.LABELS.SEG_SUFFIX))
 
-            if len(self._imgs) == 0:
-                raise RuntimeError(
-                    "Found 0 images in subfolders of: " + data_cfg.IMG_DIR if data_cfg.INDICES is not None else data_cfg.INDICES + "\n")
-        else:
-            for line in open(data_cfg.INDICES):
-                imgpath,labelpath = line.strip().split(' ')
-                self._imgs.append(os.path.join(data_cfg.IMG_DIR, imgpath))
-                self._labels.append(os.path.join(data_cfg.LABELS.DET_DIR, labelpath))
-            assert len(self._imgs) == len(self._labels), 'len(self._imgs) should be equals to len(self._labels)'
+            assert len(self._imgs) == len(self._targets), 'len(self._imgs) should be equals to len(self._targets)'
+            assert len(self._imgs) > 0, 'Found 0 images in the specified location, pls check it!'
+
+        self.coco = convert_to_coco_api(self)
 
     def __getitem__(self, idx):
-        img = Image.open(self._imgs[idx]).convert('RGB')
         if self.stage == 'infer':
-            if self.transform is not None:
-                img = torchF.to_tensor(img)
-            image_id = os.path.splitext(os.path.basename(self._imgs[idx]))[0]
-            return img, image_id
+            _img = np.asarray(Image.open(self._imgs[idx]).convert('RGB'), dtype=np.float32)
+            img_id = os.path.splitext(os.path.basename(self._imgs[idx]))[0]
+            sample = {'image': _img, 'mask': None}
+            return self.transform(sample), img_id
+        else:
+            _img, _mask = np.asarray(Image.open(self._imgs[idx]).convert('RGB'), dtype=np.float32), np.asarray(Image.open(self._targets[idx]), dtype=np.uint8)
+            _target = self.encode_map(_mask, idx)
+            sample = {'image': _img, 'target': _target}
+            return self.transform(sample)
 
-        mask = Image.open(self._labels[idx])
-        # convert the PIL Image into a numpy array
-        mask = np.array(mask)
+
+    def encode_map(self, mask, idx):
         # instances are encoded as different colors
         obj_ids = np.unique(mask)
         # first id is the background, so remove it
         obj_ids = obj_ids[1:]
-
         # split the color-encoded mask into a set
         # of binary masks
         masks = mask == obj_ids[:, None, None]
@@ -275,12 +112,19 @@ class PennFudanSegmentation(Dataset):
         target["image_id"] = image_id
         target["area"] = area
         target["iscrowd"] = iscrowd
-
-        # general resize, normalize and toTensor
-        if self.transform is not None:
-            img,target = self.transform(img,target)
-        return img, target
+        return target
 
     def __len__(self):
         return len(self._imgs)
 
+    @staticmethod
+    def collate_fn(batch):
+        '''list[tuple(Tensor, dict]'''
+        _img_list = []
+        _target_list = []
+        for bch in batch:
+            _img_list.append(bch['image'])
+            _target_list.append(bch['target'])
+        sample = {'image': _img_list, 'target': _target_list}
+        return sample
+        # return tuple(zip(*batch))
