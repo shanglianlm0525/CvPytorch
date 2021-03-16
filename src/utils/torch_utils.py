b70b1f2f8ee44468e449ebfb70d24858cb911688
mingo <569793357@qq.com>
2021-03-16 13:18:59 +0800

    torch_utils

---
+++ src/utils/torch_utils.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2021/3/8 13:55
+# @Author : liumin
+# @File : torch_utils.py
+
+import os
+import numpy as np
+import random
+import torch
+
+
+def setup_seed(seed=1029):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def get_class_name(full_class_name):
+    return full_class_name.split(".")[-1]
