7506df0aa2b63214b95658187a3dbcb5626cca2a
mingo <569793357@qq.com>
2021-03-19 10:40:56 +0800

    modify

--- src/utils/converter.py
+++ src/utils/converter.py
@@ -4,6 +4,8 @@
 # @Author : liumin
 # @File : converter.py
 
+from collections import OrderedDict
+
 
 def to_tuple(size):
     if isinstance(size, (list, tuple)):
@@ -12,3 +14,19 @@ def to_tuple(size):
         return tuple((size, size))
     else:
         raise ValueError('Unsupport data type: {}'.format(type(size)))
+
+
+def convert_state_dict(state_dict):
+    """
+    Converts a state dict saved from a dataParallel module to normal module state_dict inplace
+    Args:
+        state_dict is the loaded DataParallel model_state
+    """
+    state_dict_new = OrderedDict()
+    # print(type(state_dict))
+    for k, v in state_dict.items():
+        # print(k)
+        name = k[7:]  # remove the prefix module.
+        # My heart is borken, the pytorch have no ability to do with the problem.
+        state_dict_new[name] = v
+    return state_dict_new
