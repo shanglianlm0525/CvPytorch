f93e209bc4969b5d857c6b423e808b01aea6625a
mingo <569793357@qq.com>
2021-04-27 08:16:30 +0800

    update test

---
+++ test/quantize/static_quantization_tutorial.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2021/4/25 18:58
+# @Author : liumin
+# @File : static_quantization_tutorial.py
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import torchvision.transforms as transforms
+import os
+import time
+import sys
+import torch.quantization
+
+# # Setup warnings
+import warnings
+warnings.filterwarnings(
+    action='ignore',
+    category=DeprecationWarning,
+    module=r'.*'
+)
+warnings.filterwarnings(
+    action='default',
+    module=r'torch.quantization'
+)
+
+# Specify random seed for repeatable results
+torch.manual_seed(191009)
