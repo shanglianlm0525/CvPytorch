e57bf5b7eccf26eff874c33fde83773c93be3ba3
liumin <569793357@qq.com>
2021-06-01 15:48:50 +0800

    move out

--- compressions/quantization/post_training_quantization.py
+++
@@ -1,9 +0,0 @@
-# !/usr/bin/env python
-# -- coding: utf-8 --
-# @Time : 2021/4/26 10:34
-# @Author : liumin
-# @File : post_training_quantization.py
-
-"""
-    https://tensorflow.google.cn/lite/performance/post_training_quantization
-"""
--- compressions/quantization/quantization_aware_training.py
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/env python
-# -- coding: utf-8 --
-# @Time : 2021/4/26 10:36
-# @Author : liumin
-# @File : quantization_aware_training.py
