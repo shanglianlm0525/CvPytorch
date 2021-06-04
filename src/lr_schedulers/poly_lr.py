4b3d5be173eb33bd2f61d204d49228feb16c4468
liumin <569793357@qq.com>
2021-06-05 07:29:51 +0800

    polyLR

---
+++ src/lr_schedulers/poly_lr.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2021/6/4 8:36
+# @Author : liumin
+# @File : poly_lr.py
+
+from torch.optim.lr_scheduler import _LRScheduler, StepLR
+
+
+class PolyLR(_LRScheduler):
+    def __init__(self, optimizer, max_iters, power=0.9, last_epoch=-1, min_lr=1e-6):
+        self.power = power
+        self.max_iters = max_iters  # avoid zero lr
+        self.min_lr = min_lr
+        super(PolyLR, self).__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        return [max(base_lr * (1 - self.last_epoch / self.max_iters) ** self.power, self.min_lr)
+                for base_lr in self.base_lrs]
