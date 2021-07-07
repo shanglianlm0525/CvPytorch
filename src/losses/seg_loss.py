7e828cd74de13b52b400610513160caafb874e7f
liumin <569793357@qq.com>
2021-07-07 09:42:39 +0800

    add OhemCrossEntropyLoss2d

--- src/losses/seg_loss.py
+++ src/losses/seg_loss.py
@@ -37,7 +37,7 @@ class CrossEntropyLoss2d(nn.Module):
         assert output.shape[0] == target.shape[0] and output.shape[2:] == target.shape[1:]
         return self.CE(output, target.long())
 
-
+'''
 class OhemCrossEntropyLoss2d(nn.Module):
     def __init__(self, weight=None, thresh=0.7, min_kept=100000, ignore_index=255, reduction='mean'):
         super(OhemCrossEntropyLoss2d, self).__init__()
@@ -78,6 +78,27 @@ class OhemCrossEntropyLoss2d(nn.Module):
         target = target.masked_fill_(~valid_mask, self.ignore_index)
         target = target.view(n, h, w)
         return self.CE(output, target)
+'''
+
+
+class OhemCrossEntropyLoss2d(nn.Module):
+    def __init__(self, thresh, min_kept=100000, ignore_index=255, *args, **kwargs):
+        super(OhemCrossEntropyLoss2d, self).__init__()
+        self.thresh = -torch.log(torch.tensor(thresh, dtype=torch.float)).cuda()
+        self.min_kept = min_kept
+        self.ignore_index = ignore_index
+        self.criteria = nn.CrossEntropyLoss(ignore_index=ignore_index, reduction='none')
+
+    def forward(self, logits, labels):
+        N, C, H, W = logits.size()
+        loss = self.criteria(logits, labels.long()).view(-1)
+        loss, _ = torch.sort(loss, descending=True)
+        if loss[self.min_kept] > self.thresh:
+            loss = loss[loss>self.thresh]
+        else:
+            loss = loss[:self.min_kept]
+        return torch.mean(loss)
+
 
 
 class BCEWithLogitsLoss2d(nn.Module):
@@ -106,6 +127,7 @@ class BCEWithLogitsLoss2d(nn.Module):
         loss = F.binary_cross_entropy_with_logits(output, target.float(), weight=weight, pos_weight=pos_weight,
                                                  reduction=self.reduction)
         return loss / output.shape[0]
+
 
 class DiceLoss(nn.Module):
     def __init__(self, smooth=1., weight=None, ignore_index=255):
@@ -173,6 +195,7 @@ class LovaszSoftmax(nn.Module):
         loss = lovasz_softmax(logits, target, classes=self.classes, ignore=self.ignore_index)
         return loss
 
+
 class CE_DiceLoss(nn.Module):
     def __init__(self, smooth=1, weight=None, ignore_index=255, reduction='mean'):
         super(CE_DiceLoss, self).__init__()
@@ -184,6 +207,80 @@ class CE_DiceLoss(nn.Module):
         ce_loss = self.ce(output, target)
         dice_loss = self.dice(output, target)
         return ce_loss + dice_loss
+
+
+def dice_loss_func(input, target):
+    smooth = 1.
+    n = input.size(0)
+    iflat = input.view(n, -1)
+    tflat = target.view(n, -1)
+    intersection = (iflat * tflat).sum(1)
+    loss = 1 - ((2. * intersection + smooth) /
+                (iflat.sum(1) + tflat.sum(1) + smooth))
+    return loss.mean()
+
+
+class DetailAggregateLoss(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super(DetailAggregateLoss, self).__init__()
+
+        self.laplacian_kernel = torch.tensor(
+            [-1, -1, -1, -1, 8, -1, -1, -1, -1],
+            dtype=torch.float32).reshape(1, 1, 3, 3).requires_grad_(False).type(torch.cuda.FloatTensor)
+
+        self.fuse_kernel = torch.nn.Parameter(torch.tensor([[6. / 10], [3. / 10], [1. / 10]],
+                                                           dtype=torch.float32).reshape(1, 3, 1, 1).type(
+            torch.cuda.FloatTensor))
+
+    def forward(self, boundary_logits, gtmasks):
+        # boundary_logits = boundary_logits.unsqueeze(1)
+        boundary_targets = F.conv2d(gtmasks.unsqueeze(1).type(torch.cuda.FloatTensor), self.laplacian_kernel, padding=1)
+        boundary_targets = boundary_targets.clamp(min=0)
+        boundary_targets[boundary_targets > 0.1] = 1
+        boundary_targets[boundary_targets <= 0.1] = 0
+
+        boundary_targets_x2 = F.conv2d(gtmasks.unsqueeze(1).type(torch.cuda.FloatTensor), self.laplacian_kernel,
+                                       stride=2, padding=1)
+        boundary_targets_x2 = boundary_targets_x2.clamp(min=0)
+
+        boundary_targets_x4 = F.conv2d(gtmasks.unsqueeze(1).type(torch.cuda.FloatTensor), self.laplacian_kernel,
+                                       stride=4, padding=1)
+        boundary_targets_x4 = boundary_targets_x4.clamp(min=0)
+
+        boundary_targets_x8 = F.conv2d(gtmasks.unsqueeze(1).type(torch.cuda.FloatTensor), self.laplacian_kernel,
+                                       stride=8, padding=1)
+        boundary_targets_x8 = boundary_targets_x8.clamp(min=0)
+
+        boundary_targets_x8_up = F.interpolate(boundary_targets_x8, boundary_targets.shape[2:], mode='nearest')
+        boundary_targets_x4_up = F.interpolate(boundary_targets_x4, boundary_targets.shape[2:], mode='nearest')
+        boundary_targets_x2_up = F.interpolate(boundary_targets_x2, boundary_targets.shape[2:], mode='nearest')
+
+        boundary_targets_x2_up[boundary_targets_x2_up > 0.1] = 1
+        boundary_targets_x2_up[boundary_targets_x2_up <= 0.1] = 0
+
+        boundary_targets_x4_up[boundary_targets_x4_up > 0.1] = 1
+        boundary_targets_x4_up[boundary_targets_x4_up <= 0.1] = 0
+
+        boundary_targets_x8_up[boundary_targets_x8_up > 0.1] = 1
+        boundary_targets_x8_up[boundary_targets_x8_up <= 0.1] = 0
+
+        boudary_targets_pyramids = torch.stack((boundary_targets, boundary_targets_x2_up, boundary_targets_x4_up),
+                                               dim=1)
+
+        boudary_targets_pyramids = boudary_targets_pyramids.squeeze(2)
+        boudary_targets_pyramid = F.conv2d(boudary_targets_pyramids, self.fuse_kernel)
+
+        boudary_targets_pyramid[boudary_targets_pyramid > 0.1] = 1
+        boudary_targets_pyramid[boudary_targets_pyramid <= 0.1] = 0
+
+        if boundary_logits.shape[-1] != boundary_targets.shape[-1]:
+            boundary_logits = F.interpolate(
+                boundary_logits, boundary_targets.shape[2:], mode='bilinear', align_corners=True)
+
+        bce_loss = F.binary_cross_entropy_with_logits(boundary_logits, boudary_targets_pyramid)
+        dice_loss = dice_loss_func(torch.sigmoid(boundary_logits), boudary_targets_pyramid)
+        return bce_loss, dice_loss
+
 
 
 if __name__ == '__main__':
