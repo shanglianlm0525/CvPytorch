060bf7b728b37e0e88999dc5c89326234c1c5c38
liumin <569793357@qq.com>
2021-07-09 13:44:57 +0800

    finish nanodet coco

--- src/models/nanodet.py
+++ src/models/nanodet.py
@@ -14,31 +14,11 @@ from .necks import build_neck
 from .heads import build_head
 
 
-def warp_boxes(boxes, M, width, height):
-    n = len(boxes)
-    if n:
-        # warp points
-        xy = np.ones((n * 4, 3))
-        xy[:, :2] = boxes[:, [0, 1, 2, 3, 0, 3, 2, 1]].reshape(n * 4, 2)  # x1y1, x2y2, x1y2, x2y1
-        xy = xy @ M.T  # transform
-        xy = (xy[:, :2] / xy[:, 2:3]).reshape(n, 8)  # rescale
-        # create new boxes
-        x = xy[:, [0, 2, 4, 6]]
-        y = xy[:, [1, 3, 5, 7]]
-        xy = np.concatenate((x.min(1), y.min(1), x.max(1), y.max(1))).reshape(4, n).T
-        # clip boxes
-        xy[:, [0, 2]] = xy[:, [0, 2]].clip(0, width)
-        xy[:, [1, 3]] = xy[:, [1, 3]].clip(0, height)
-        return xy.astype(np.float32)
-    else:
-        return boxes
-
-
 class NanoDet(nn.Module):
     def __init__(self, dictionary=None):
         super(NanoDet, self).__init__()
         self.dictionary = dictionary
-        self.dummy_input = torch.zeros(1, 3, 480, 360)
+        self.dummy_input = torch.zeros(1, 3, 320, 320)
 
         self.num_classes = len(self.dictionary)
         self.category = [v for d in self.dictionary for v in d.keys()]
@@ -98,20 +78,20 @@ class NanoDet(nn.Module):
     def trans_specific_format(self, imgs, targets):
         new_boxes = []
         new_labels = []
-        new_warp_matrixs = []
+        new_scales = []
         new_heights = []
         new_widths = []
         for target in targets:
             new_boxes.append(target['boxes'])
             new_labels.append(target['labels'])
-            new_warp_matrixs.append(target['warp_matrix'])
+            new_scales.append(target['scales'])
             new_heights.append(target['height'])
             new_widths.append(target['width'])
 
         t_targets = {}
         t_targets["boxes"] = new_boxes
         t_targets["labels"] = new_labels
-        t_targets["warp_matrix"] = new_warp_matrixs
+        t_targets["scales"] = new_scales
         t_targets["height"] = new_heights
         t_targets["width"] = new_widths
         return torch.stack(imgs), t_targets
@@ -137,35 +117,19 @@ class NanoDet(nn.Module):
             if mode == 'val':
                 outputs = []
                 dets = self.head.post_process(preds, imgs)
-                # print('dets', dets)
-                for warp_matrix, width, height, det in zip(targets['warp_matrix'], targets['width'], targets['height'], dets):
+                for width, height, scale, det in zip(targets['width'], targets['height'], targets['scales'],dets):
                     det_bboxes, det_labels = det
                     det_bboxes_np = det_bboxes[:, :4].cpu().numpy()
-                    warp_matrix = warp_matrix.cpu().numpy()
                     width = width.cpu().numpy()
                     height = height.cpu().numpy()
-                    det_bboxes_np = warp_boxes(det_bboxes_np, np.linalg.inv(warp_matrix), width, height)
+                    scale_h = scale[0].cpu().numpy()
+                    scale_w = scale[1].cpu().numpy()
+                    det_bboxes_np[:, [0, 2]] /= scale_w
+                    det_bboxes_np[:, [1, 3]] /= scale_h
+                    # clip boxes
+                    det_bboxes_np[:, [0, 2]] = det_bboxes_np[:, [0, 2]].clip(0, width)
+                    det_bboxes_np[:, [1, 3]] = det_bboxes_np[:, [1, 3]].clip(0, height)
                     outputs.append({"boxes": torch.tensor(det_bboxes_np), "labels": det_labels, "scores": det_bboxes[:, 4]})
-
                 return losses, outputs
             else:
                 return losses
-
-    def test234(self, result_list, meta):
-        preds = {}
-        warp_matrix = meta['warp_matrix'][0] if isinstance(meta['warp_matrix'], list) else meta['warp_matrix']
-        img_height = meta['img_info']['height'].cpu().numpy() \
-            if isinstance(meta['img_info']['height'], torch.Tensor) else meta['img_info']['height']
-        img_width = meta['img_info']['width'].cpu().numpy() \
-            if isinstance(meta['img_info']['width'], torch.Tensor) else meta['img_info']['width']
-        for result in result_list:
-            det_bboxes, det_labels = result
-            det_bboxes = det_bboxes.cpu().numpy()
-            det_bboxes[:, :4] = warp_boxes(det_bboxes[:, :4], np.linalg.inv(warp_matrix), img_width, img_height)
-            classes = det_labels.cpu().numpy()
-            for i in range(self.num_classes):
-                inds = (classes == i)
-                preds[i] = np.concatenate([
-                    det_bboxes[inds, :4].astype(np.float32),
-                    det_bboxes[inds, 4:5].astype(np.float32)], axis=1).tolist()
-        return preds
