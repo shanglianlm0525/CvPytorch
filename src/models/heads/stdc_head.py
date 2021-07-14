260460c0c011eb41598bc6935c96ef3f8a9f0624
liumin <569793357@qq.com>
2021-07-14 08:33:04 +0800

    add stdc_head

--- src/models/heads/stdc_head.py
+++ src/models/heads/stdc_head.py
@@ -10,29 +10,7 @@ import torch.nn.functional as F
 
 from src.models.modules.convs import ConvModule
 
-'''
-class ConvBNReLU(nn.Module):
-    def __init__(self, in_chan, out_chan, ks=3, stride=1, padding=1, *args, **kwargs):
-        super(ConvBNReLU, self).__init__()
-        self.conv = nn.Conv2d(in_chan, out_chan, kernel_size = ks, stride = stride, padding = padding, bias = False)
-        self.bn = nn.BatchNorm2d(out_chan)
-        # self.bn = BatchNorm2d(out_chan, activation='none')
-        self.relu = nn.ReLU()
-        self.init_weight()
 
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        return x
-
-    def init_weight(self):
-        for ly in self.children():
-            if isinstance(ly, nn.Conv2d):
-                nn.init.kaiming_normal_(ly.weight, a=1)
-                if ly.bias is not None:
-                    nn.init.constant_(ly.bias, 0)
-'''
 
 class AttentionRefinementModule(nn.Module):
     def __init__(self, in_channel, out_channel, *args, **kwargs):
