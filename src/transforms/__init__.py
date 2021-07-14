37e0598e8cd1491abc2a1e094571112036d4fa0d
liumin <569793357@qq.com>
2021-07-14 08:32:25 +0800

    add build target transform

--- src/transforms/__init__.py
+++ src/transforms/__init__.py
@@ -9,7 +9,10 @@ import src.transforms.cls_transforms as cls_t
 import src.transforms.seg_transforms as seg_t
 import src.transforms.det_transforms as det_t
 import src.transforms.ins_transforms as ins_t
-
+import src.transforms.cls_target_transforms as cls_target_t
+import src.transforms.seg_target_transforms as seg_target_t
+import src.transforms.det_target_transforms as det_target_t
+import src.transforms.ins_target_transforms as ins_target_t
 
 def build_transforms(dict_name, cfg, mode='train'):
     _params = []
@@ -22,6 +25,27 @@ def build_transforms(dict_name, cfg, mode='train'):
         trans = det_t
     elif dict_name == 'INS_CLASSES':
         trans = ins_t
+    else:
+        raise ValueError("Unsupported transforms type: {}".format(dict_name))
+
+    transform_cfg = copy.deepcopy(cfg)
+    for t, v in transform_cfg.items():
+        t = getattr(trans, t)(**v) if v is not None else getattr(trans, t)()
+        _params.append(t)
+    return trans.Compose(_params)
+
+
+def build_targets_transforms(dict_name, cfg, mode='train'):
+    _params = []
+
+    if dict_name == 'CLS_CLASSES':
+        trans = cls_target_t
+    elif dict_name == 'SEG_CLASSES':
+        trans = seg_target_t
+    elif dict_name == 'DET_CLASSES':
+        trans = det_target_t
+    elif dict_name == 'INS_CLASSES':
+        trans = ins_target_t
     else:
         raise ValueError("Unsupported transforms type: {}".format(dict_name))
 
