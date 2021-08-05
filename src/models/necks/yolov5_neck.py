36a9ee1c5cd09005a47299b7e81110fd1e0f97a6
liumin <569793357@qq.com>
2021-08-05 08:26:31 +0800

    add depth_mul and  width_mul factor

--- src/models/necks/yolov5_neck.py
+++ src/models/necks/yolov5_neck.py
@@ -7,34 +7,33 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from ..modules.yolov5_modules import C3, Conv
+# from ..modules.yolov5_modules import C3, Conv
+from src.models.modules.yolov5_modules import Conv, C3
 
 
 class YOLOv5Neck(nn.Module):
     def __init__(self, in_channels, out_channels, layers=[3, 3, 3], depth_mul=1.0, width_mul=1.0):
         super(YOLOv5Neck, self).__init__()
         assert isinstance(in_channels, list)
-        self.in_channels = in_channels
-        self.out_channels = out_channels
         self.width_mul = width_mul
         self.depth_mul = depth_mul
+        self.in_channels = list(map(lambda x: max(round(x * self.width_mul), 1), in_channels))
+        self.out_channels = list(map(lambda x: max(round(x * self.width_mul), 1), out_channels))
         self.num_ins = len(in_channels)
-
-        self.block_nums = [3, 3, 3]
-        layers = list(map(lambda x: max(round(x * self.depth_mul), 1), layers))
+        self.layers = list(map(lambda x: max(round(x * self.depth_mul), 1), layers))
 
         self.up_convs = nn.ModuleList()
         for i in range(self.num_ins-1, 0, -1):
             if i == self.num_ins-1:
                 up_conv = nn.Sequential(
-                    C3(in_channels[i], in_channels[i], layers[i], False),
-                    Conv(in_channels[i], in_channels[i-1], 1, 1, 0)
+                    C3(self.in_channels[i], self.in_channels[i], self.layers[i], False),
+                    Conv(self.in_channels[i], self.in_channels[i-1], 1, 1, 0)
                 )
                 self.up_convs.append(up_conv)
             else:
                 up_conv = nn.Sequential(
-                    C3(in_channels[i] * 2, in_channels[i], layers[i], False),
-                    Conv(in_channels[i], in_channels[i-1], 1, 1, 0)
+                    C3(self.in_channels[i] * 2, self.in_channels[i], self.layers[i], False),
+                    Conv(self.in_channels[i], self.in_channels[i-1], 1, 1, 0)
                 )
                 self.up_convs.append(up_conv)
 
@@ -43,11 +42,11 @@ class YOLOv5Neck(nn.Module):
         self.down_convs2 = nn.ModuleList()
         for i in range(self.num_ins):
             if i == 0:
-                self.down_convs1.append(C3(in_channels[i] * 2, out_channels[i], 3, False))
+                self.down_convs1.append(C3(self.in_channels[i] * 2, self.out_channels[i], 3, False))
             else:
-                self.down_convs1.append(C3(in_channels[i], out_channels[i], 3, False))
+                self.down_convs1.append(C3(self.in_channels[i], self.out_channels[i], 3, False))
             if i < self.num_ins-1:
-                self.down_convs2.append(Conv(out_channels[i], in_channels[i], 3, 2, 1))
+                self.down_convs2.append(Conv(self.out_channels[i], self.in_channels[i], 3, 2, 1))
 
         self.init_weights()
 
@@ -66,7 +65,7 @@ class YOLOv5Neck(nn.Module):
 
     def forward(self, x):
         assert len(x) == len(self.in_channels)
-        outs = x
+        outs = list(x)
         used_backbone_levels = len(outs)
         for i in range(used_backbone_levels - 1, 0, -1):
             outs[i] = self.up_convs[used_backbone_levels - 1 - i](outs[i])
@@ -91,7 +90,7 @@ if __name__ == "__main__":
     inputs = [torch.rand(1, c, s, s) for c, s in zip(in_channels, scales)]
     for i in range(len(inputs)):
         print(f'inputs[{i}].shape = {inputs[i].shape}')
-    self = YOLOv5Neck(in_channels, out_channels, depth_mul = 0.33, width_mul = 0.50).eval()
+    self = YOLOv5Neck(in_channels, out_channels, depth_mul = 1, width_mul = 1).eval()
     print(self)
     outputs = self.forward(inputs)
     for i in range(len(outputs)):
