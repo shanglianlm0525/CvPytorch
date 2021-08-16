b214d4f12d3989e7142d01a058865e6e3a0c0639
liumin <569793357@qq.com>
2021-08-16 18:21:01 +0800

    remove torch.stack(imgs, 0)

--- src/models/yolov5.py
+++ src/models/yolov5.py
@@ -241,7 +241,7 @@ class YOLOv5(nn.Module):
         t_targets["pads"] = new_pads if len(new_pads) > 0 else []
         t_targets["height"] = new_heights
         t_targets["width"] = new_widths
-        return torch.stack(imgs), t_targets
+        return imgs, t_targets
 
     def forward(self, imgs, targets=None, mode='infer', **kwargs):
 
