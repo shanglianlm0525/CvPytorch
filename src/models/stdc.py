15db2527ba102df19700f37a84d4834d6737d468
liumin <569793357@qq.com>
2021-09-06 09:07:28 +0800

    update

--- src/models/stdc.py
+++ src/models/stdc.py
@@ -42,9 +42,10 @@ class STDC(nn.Module):
         head_cfg = {'name': 'StdcHead', 'in_channels': self.backbone.out_channels, 'num_classes': self.num_classes}
         self.head = build_head(head_cfg)
         '''
+        self.model_cfg.HEAD.__setitem__('num_classes', self.num_classes)
 
         self.backbone = build_backbone(self.model_cfg.BACKBONE)
-        self.head = build_head(self.num_classes, self.model_cfg.HEAD)
+        self.head = build_head(self.model_cfg.HEAD)
 
         self.ce_criterion = OhemCrossEntropyLoss2d(thresh=0.7).cuda()
         self.boundary_criterion = DetailAggregateLoss().cuda()
