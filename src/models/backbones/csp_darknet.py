f0bc5276fd21550c51d2beffd73b621b1376814f
liumin <569793357@qq.com>
2021-09-10 08:50:42 +0800

    update

--- src/models/backbones/csp_darknet.py
+++ src/models/backbones/csp_darknet.py
@@ -1,41 +1,39 @@
 # !/usr/bin/env python
 # -- coding: utf-8 --
-# @Time : 2021/7/26 10:35
+# @Time : 2021/9/7 9:39
 # @Author : liumin
 # @File : csp_darknet.py
 
 import torch
 import torch.nn as nn
 
-from src.models.modules.yolox_modules import BaseConv, DWConv, Focus, CSPLayer, SPPBottleneck
+from src.models.modules.yolov5_modules import Focus, BottleneckCSP, Conv, SPP
 
 """
-    YOLOX: Exceeding YOLO Series in 2021
-    https://arxiv.org/pdf/2107.08430.pdf
+    YOLOv4: Optimal Speed and Accuracy of Object Detection
+    https://arxiv.org/pdf/2004.10934.pdf
 """
-
 
 class CspDarkNet(nn.Module):
-
-    def __init__(self, subtype='yolox_s', out_stages=[2,3,4], output_stride=32, depthwise = False, backbone_path=None, pretrained=False):
+    def __init__(self, subtype='cspdark_s', out_stages=[2, 3, 4], output_stride=32, backbone_path=None,
+                 pretrained=False):
         super(CspDarkNet, self).__init__()
         self.subtype = subtype
         self.out_stages = out_stages
+        self.output_stride = output_stride
         self.backbone_path = backbone_path
         self.pretrained = pretrained
 
-        Conv = DWConv if depthwise else BaseConv
-
-        if self.subtype == 'yolox_s':
+        if self.subtype == 'cspdark_s':
             dep_mul, wid_mul = 0.33, 0.5
             self.out_channels = [32, 64, 128, 256, 512]
-        elif self.subtype == 'yolox_m':
+        elif self.subtype == 'cspdark_m':
             dep_mul, wid_mul = 0.67, 0.75
             self.out_channels = [48, 96, 192, 384, 768]
-        elif self.subtype == 'yolox_l':
+        elif self.subtype == 'cspdark_l':
             dep_mul, wid_mul = 1.0, 1.0
             self.out_channels = [64, 128, 256, 512, 1024]
-        elif self.subtype == 'yolox_x':
+        elif self.subtype == 'cspdark_x':
             dep_mul, wid_mul = 1.33, 1.25
             self.out_channels = [80, 160, 320, 640, 1280]
         else:
@@ -45,37 +43,36 @@ class CspDarkNet(nn.Module):
         base_depth = max(round(dep_mul * 3), 1)  # 3
 
         # stem
-        self.stem = Focus(3, base_channels, ksize=3)
+        self.stem = Focus(3, base_channels, k=3)
 
         # dark2
         self.stage1 = nn.Sequential(
             Conv(base_channels, base_channels * 2, 3, 2),
-            CSPLayer(base_channels * 2, base_channels * 2, n=base_depth, depthwise=depthwise),
+            BottleneckCSP(base_channels * 2, base_channels * 2, n=base_depth),
         )
 
         # dark3
         self.stage2 = nn.Sequential(
             Conv(base_channels * 2, base_channels * 4, 3, 2),
-            CSPLayer(base_channels * 4, base_channels * 4, n=base_depth * 3, depthwise=depthwise),
+            BottleneckCSP(base_channels * 4, base_channels * 4, n=base_depth * 3),
         )
 
         # dark4
         self.stage3 = nn.Sequential(
             Conv(base_channels * 4, base_channels * 8, 3, 2),
-            CSPLayer(base_channels * 8, base_channels * 8, n=base_depth * 3, depthwise=depthwise),
+            BottleneckCSP(base_channels * 8, base_channels * 8, n=base_depth * 3),
         )
 
         # dark5
         self.stage4 = nn.Sequential(
             Conv(base_channels * 8, base_channels * 16, 3, 2),
-            SPPBottleneck(base_channels * 16, base_channels * 16),
-            CSPLayer(base_channels * 16, base_channels * 16, n=base_depth, shortcut=False, depthwise=depthwise),
+            SPP(base_channels * 16, base_channels * 16),
+            BottleneckCSP(base_channels * 16, base_channels * 16, n=base_depth, shortcut=False),
         )
 
         self.out_channels = self.out_channels[self.out_stages[0]:self.out_stages[-1] + 1]
 
         self.init_weights()
-
 
     def forward(self, x):
         x = self.stem(x)
@@ -86,7 +83,6 @@ class CspDarkNet(nn.Module):
             if i in self.out_stages:
                 output.append(x)
         return tuple(output) if len(self.out_stages) > 1 else output[0]
-
 
     def init_weights(self):
         for name, m in self.named_modules():
@@ -111,7 +107,7 @@ class CspDarkNet(nn.Module):
 
 
 if __name__ == "__main__":
-    model = CspDarkNet('yolox_l')
+    model = CspDarkNet('cspdark_s')
     print(model)
 
     input = torch.randn(1, 3, 640, 640)
