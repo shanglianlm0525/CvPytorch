f9f625b80fdb37ad32a2df9262170ffb997391e6
liumin <569793357@qq.com>
2021-10-09 08:50:41 +0800

    rebuild logger for customize

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,36 +1,42 @@
-import json
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2021/9/29 14:00
+# @Author : liumin
+# @File : logger.py
+
 import os
 import logging
 import datetime
 from termcolor import colored
 
-LOG_DIR = 'logs'
-LOG_LEVEL = os.environ.get('LOG_LEVEL') or 'INFO'
-logger = logging.getLogger('pytorch')
-logger.setLevel(LOG_LEVEL)
-logger.handlers = []
-logger.propagate = 0
 
-if not os.path.exists(LOG_DIR):
-    os.makedirs(LOG_DIR)
+def getLogger(save_dir=None, experiment_id=None):
+    # LOG_DIR = 'logs'
+    LOG_DIR = os.path.join(save_dir, experiment_id)
+    LOG_LEVEL = os.environ.get('LOG_LEVEL') or 'INFO'
+    logger = logging.getLogger('pytorch')
+    logger.setLevel(LOG_LEVEL)
+    logger.handlers = []
+    logger.propagate = 0
 
-fileHandeler = logging.FileHandler(os.path.sep.join([
-    LOG_DIR if os.path.isdir(LOG_DIR) and os.access(LOG_DIR, os.W_OK) else '/logs',
-    datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d')
-]))
-fileHandeler.setLevel(LOG_LEVEL)
+    if not os.path.exists(LOG_DIR):
+        os.makedirs(LOG_DIR)
 
-consoleHandeler = logging.StreamHandler()
-consoleHandeler.setLevel(LOG_LEVEL)
-fileFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-fmt = colored('%(asctime)s', 'blue') +' - '+ colored('%(name)s', 'magenta', attrs=['bold']) + ' - '+ \
-              colored('%(levelname)s:', 'green') +' - '+ colored('%(message)s', 'white')
-consoleFormatter = logging.Formatter(fmt, datefmt="%Y-%m-%d %H:%M:%S")
-fileHandeler.setFormatter(fileFormatter)
-consoleHandeler.setFormatter(consoleFormatter)
+    fileHandeler = logging.FileHandler(os.path.sep.join([
+        LOG_DIR if os.path.isdir(LOG_DIR) and os.access(LOG_DIR, os.W_OK) else LOG_DIR, 'log'
+    ]))
+    fileHandeler.setLevel(LOG_LEVEL)
 
-logger.addHandler(fileHandeler)
-logger.addHandler(consoleHandeler)
+    consoleHandeler = logging.StreamHandler()
+    consoleHandeler.setLevel(LOG_LEVEL)
+    fileFormatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fmt = colored('%(asctime)s', 'blue') + ' - ' + colored('%(name)s', 'magenta', attrs=['bold']) + ' - ' + \
+          colored('%(levelname)s:', 'green') + ' - ' + colored('%(message)s', 'white')
+    consoleFormatter = logging.Formatter(fmt, datefmt="%Y-%m-%d %H:%M:%S")
+    fileHandeler.setFormatter(fileFormatter)
+    consoleHandeler.setFormatter(consoleFormatter)
 
+    logger.addHandler(fileHandeler)
+    logger.addHandler(consoleHandeler)
 
-
+    return logger
