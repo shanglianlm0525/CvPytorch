52336e76607c7261ceb98535db2d460b4b2501ac
liumin <569793357@qq.com>
2021-11-04 08:40:03 +0800

    rename

---
+++ src/losses/__init__.py
@@ -0,0 +1,38 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2021/11/2 13:21
+# @Author : liumin
+# @File : __init__.py
+
+from copy import deepcopy
+
+from src.losses.yolofastestv2_loss import YoloFastestv2Loss
+from src.losses.yolop_loss import YolopLoss
+from src.losses.yolov5_loss import Yolov5Loss
+from src.losses.efficientdet_loss import EfficientDetLoss
+from src.losses.yolox_loss import YoloxLoss
+
+
+__all__ = ['Yolov5Loss', 'EfficientDetLoss', 'YoloFastestv2Loss', 'YoloxLoss',
+           'YolopLoss']
+
+
+
+def build_loss(cfg):
+    loss_cfg = deepcopy(cfg)
+    name = loss_cfg.pop('name')
+
+    if name == 'Yolov5Loss':
+        return Yolov5Loss(**loss_cfg)
+    elif name == 'EfficientDetLoss':
+        return EfficientDetLoss(**loss_cfg)
+    elif name == 'YoloFastestv2Loss':
+        return YoloFastestv2Loss(**loss_cfg)
+    elif name == 'YoloxLoss':
+        return YoloxLoss(**loss_cfg)
+
+
+    elif name == 'YolopLoss':
+        return YolopLoss(**loss_cfg)
+    else:
+        raise NotImplementedError
