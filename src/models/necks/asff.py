814459049ea4e071b8a71caa11d16c4727945822
liumin <569793357@qq.com>
2021-11-12 16:27:07 +0800

    update

--- src/models/necks/asff.py
+++ src/models/necks/asff.py
@@ -4,12 +4,15 @@
 # @Author : liumin
 # @File : asff.py
 
+import torch
+import torch.nn as nn
+
 """
     Learning Spatial Fusion for Single-Shot Object Detection
     https://arxiv.org/pdf/1911.09516.pdf
 """
 
-class ASFF():
+class ASFF(nn.Module):
     def __init__(self):
         super(ASFF, self).__init__()
         pass
