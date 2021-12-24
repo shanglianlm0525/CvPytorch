4571f0d655a73cb9d1034c657687bf0f4865d4ff
liumin <569793357@qq.com>
2021-12-24 08:38:04 +0800

    update

--- src/data/transforms/det_transforms.py
+++ src/data/transforms/det_transforms.py
@@ -8,6 +8,7 @@ import math
 import random
 import warnings
 
+
 import cv2
 import numpy as np
 import torch
@@ -1186,6 +1187,65 @@ class EfficientDetAugment(object):
         target["boxes"] = torch.from_numpy(boxes)
         return {'image': torch.from_numpy(new_img.astype(np.float32)), 'target': target}
 
+
+class FcosAugment(object):
+    def __init__(self, augment=True):
+        self.augment = augment
+
+    def __call__(self, sample):
+        img, target = sample['image'], sample['target']
+        boxes = target["boxes"]
+        labels = target["labels"]
+
+        img = Image.fromarray(img.astype(np.uint8))
+        if random.random() < 0.3:
+            # img, boxes = colorJitter(img, boxes)
+            img = T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1)(img)
+        if random.random() < 0.5:
+            # img, boxes = random_rotation(img, boxes)
+            degree = 10
+            d = random.uniform(-degree, degree)
+            w, h = img.size
+            rx0, ry0 = w / 2.0, h / 2.0
+            img = img.rotate(d)
+            a = -d / 180.0 * math.pi
+
+            new_boxes = np.zeros_like(boxes)
+            new_boxes[:, 0] = boxes[:, 1]
+            new_boxes[:, 1] = boxes[:, 0]
+            new_boxes[:, 2] = boxes[:, 3]
+            new_boxes[:, 3] = boxes[:, 2]
+            for i in range(boxes.shape[0]):
+                ymin, xmin, ymax, xmax = new_boxes[i, :]
+                xmin, ymin, xmax, ymax = float(xmin), float(ymin), float(xmax), float(ymax)
+                x0, y0 = xmin, ymin
+                x1, y1 = xmin, ymax
+                x2, y2 = xmax, ymin
+                x3, y3 = xmax, ymax
+
+                z = np.array([[y0, x0], [y1, x1], [y2, x2], [y3, x3]])
+                tp = np.zeros_like(z)
+                tp[:, 1] = (z[:, 1] - rx0) * math.cos(a) - (z[:, 0] - ry0) * math.sin(a) + rx0
+                tp[:, 0] = (z[:, 1] - rx0) * math.sin(a) + (z[:, 0] - ry0) * math.cos(a) + ry0
+
+                ymax, xmax = np.max(tp, axis=0)
+                ymin, xmin = np.min(tp, axis=0)
+                new_boxes[i] = np.array([ymin, xmin, ymax, xmax])
+            np.clip(new_boxes[:, 1::2], 0, w - 1)
+            np.clip(new_boxes[:, 0::2], 0, h - 1)
+            boxes[:, 0] = new_boxes[:, 1]
+            boxes[:, 1] = new_boxes[:, 0]
+            boxes[:, 2] = new_boxes[:, 3]
+            boxes[:, 3] = new_boxes[:, 2]
+
+        if random.random() < 0.5:
+            # img, boxes = random_crop_resize(img, boxes)
+            pass
+
+        img = np.array(img)
+        target["labels"] = labels
+        target["boxes"] = boxes
+        return {'image': img, 'target': target}
 
 
 class YOLOv5AugWithoutAugment(object):
