4be6c32c2a02dc7d2dc1c9db3dd8e8765cbaa231
liumin <569793357@qq.com>
2022-05-08 17:26:39 +0800

    update

--- src/models/heads/fcos_head.py
+++ src/models/heads/fcos_head.py
@@ -14,6 +14,7 @@ class ScaleExp(nn.Module):
     def __init__(self,init_value=1.0):
         super(ScaleExp,self).__init__()
         self.scale=nn.Parameter(torch.tensor([init_value],dtype=torch.float32))
+
     def forward(self,x):
         return torch.exp(x*self.scale)
 
@@ -53,12 +54,12 @@ class FCOSHead(nn.Module):
         self.cnt_logits = nn.Conv2d(in_channel, 1, kernel_size=3, padding=1)
         self.reg_pred = nn.Conv2d(in_channel, 4, kernel_size=3, padding=1)
 
-        self.apply(self.init_conv_RandomNormal)
+        self.apply(self.init_weights)
 
         nn.init.constant_(self.cls_logits.bias, -math.log((1 - prior) / prior))
         self.scale_exp = nn.ModuleList([ScaleExp(1.0) for _ in range(5)])
 
-    def init_conv_RandomNormal(self, module, std=0.01):
+    def init_weights(self, module, std=0.01):
         if isinstance(module, nn.Conv2d):
             nn.init.normal_(module.weight, std=std)
 
