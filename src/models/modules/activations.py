e02afc89c43850d9dd6ace34aa70e98695afb31f
liumin <569793357@qq.com>
2022-05-08 17:25:57 +0800

    add CReLU

--- src/models/modules/activations.py
+++ src/models/modules/activations.py
@@ -31,6 +31,14 @@ class Swish(nn.Module):
         return x * torch.sigmoid(x)
 
 
+class CReLU(nn.Module):
+    def __init__(self, inplace=True):
+        super().__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(torch.cat([x, -x], dim=1))
+
 
 activations = {'ReLU': nn.ReLU,
                'LeakyReLU': nn.LeakyReLU,
@@ -39,10 +47,10 @@ activations = {'ReLU': nn.ReLU,
                'ELU': nn.ELU,
                'GELU': nn.GELU,
                'Swish': Swish,
+               'CReLU': CReLU,
                'MemoryEfficientSwish': MemoryEfficientSwish,
                 None: nn.Identity
                }
-
 
 def act_layers(name):
     assert name in activations.keys()
