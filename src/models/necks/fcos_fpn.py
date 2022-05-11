cbddde8dc96413f18db5b93b6c7e151b4334e98c
liumin <569793357@qq.com>
2022-05-11 10:44:07 +0800

    update

--- src/models/necks/fcos_fpn.py
+++ src/models/necks/fcos_fpn.py
@@ -12,25 +12,25 @@ import math
 class FCOSFPN(nn.Module):
     '''only for resnet50,101,152'''
 
-    def __init__(self, out_channels=256):
+    def __init__(self, in_channels=[512, 1024, 2048], out_channels=256):
         super(FCOSFPN, self).__init__()
-        self.prj_5 = nn.Conv2d(2048, out_channels, kernel_size=1)
-        self.prj_4 = nn.Conv2d(1024, out_channels, kernel_size=1)
-        self.prj_3 = nn.Conv2d(512, out_channels, kernel_size=1)
-        self.conv_5 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.conv_4 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        self.conv_3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.prj_3 = nn.Conv2d(in_channels[0], out_channels, kernel_size=1)
+        self.prj_4 = nn.Conv2d(in_channels[1], out_channels, kernel_size=1)
+        self.prj_5 = nn.Conv2d(in_channels[2], out_channels, kernel_size=1)
+        self.conv_5 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv_4 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv_3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
 
-        self.conv_out6 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=2)
-        self.conv_out7 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, stride=2)
+        self.conv_out6 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1)
+        self.conv_out7 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1)
 
-        self.init_conv_kaiming()
+        self.init_weights()
 
     def upsamplelike(self, inputs):
         src, target = inputs
         return F.interpolate(src, size=(target.shape[2], target.shape[3]), mode='nearest')
 
-    def init_conv_kaiming(self):
+    def init_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_uniform_(m.weight, a=1)
@@ -39,9 +39,9 @@ class FCOSFPN(nn.Module):
 
     def forward(self, x):
         C3, C4, C5 = x
-        P5 = self.prj_5(C5)
-        P4 = self.prj_4(C4)
         P3 = self.prj_3(C3)
+        P4 = self.prj_4(C4)
+        P5 = self.prj_5(C5)
 
         P4 = P4 + self.upsamplelike([P5, C4])
         P3 = P3 + self.upsamplelike([P4, C3])
