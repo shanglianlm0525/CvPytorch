fa23e36f9c49c0a2fa0112b7ddfc5176ab32b47d
liumin <569793357@qq.com>
2022-07-01 09:57:04 +0800

    update

--- src/models/modules/yolov5_modules.py
+++ src/models/modules/yolov5_modules.py
@@ -404,19 +404,20 @@ class Detect(nn.Module):
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
             x[i] = x[i].view(bs, self.num_anchors, self.num_outputs, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
-            # inference
-            if self.grid[i].shape[2:4] != x[i].shape[2:4]:
-                self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
+            if not self.training:  # inference
+                if self.grid[i].shape[2:4] != x[i].shape[2:4]:
+                    self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
 
-            y = x[i].sigmoid()
+                y = x[i].sigmoid()
 
-            y[..., 0:2] = (y[..., 0:2] * 2 - 0.5 + self.grid[i]) * self.stride[i]  # xy
-            y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                y[..., 0:2] = (y[..., 0:2] * 2 - 0.5 + self.grid[i]) * self.stride[i]  # xy
+                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
 
-            z.append(y.view(bs, -1, self.num_outputs))
+                z.append(y.view(bs, -1, self.num_outputs))
 
         # return x if self.training else (torch.cat(z, 1), x)
-        return torch.cat(z, 1), x
+        # return torch.cat(z, 1), x
+        return (None, x) if self.training else (torch.cat(z, 1), None)
 
 
     def _make_grid(self, nx=20, ny=20, i=0):
