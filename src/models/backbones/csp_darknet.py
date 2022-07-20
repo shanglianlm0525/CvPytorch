a074365e810a86ae51d8d938847106d531c2ba78
liumin <569793357@qq.com>
2022-07-20 08:34:46 +0800

    add depth_mul and width_mul

--- src/models/backbones/csp_darknet.py
+++ src/models/backbones/csp_darknet.py
@@ -14,16 +14,12 @@ from src.models.modules.yolox_modules import Focus, BaseConv, CSPLayer, SPPBottl
     https://arxiv.org/pdf/2004.10934.pdf
     YOLOX: Exceeding YOLO Series in 2021
     https://arxiv.org/pdf/2107.08430.pdf
+    AIRDet:
+    https://github.com/tinyvision/AIRDet
 """
 
 class CspDarkNet(nn.Module):
-    cfg = {"nano": [0.33, 0.25],
-            "tiny": [0.33, 0.375],
-            "s": [0.33, 0.5],
-            "m": [0.67, 0.75],
-            "l": [1.0, 1.0],
-            "x": [1.33, 1.25]}
-    def __init__(self, subtype='cspdark_s', out_stages=[2, 3, 4], output_stride=32, backbone_path=None,
+    def __init__(self, subtype='cspdark_s', out_stages=[2, 3, 4], output_stride=32, depth_mul=1.0, width_mul=1.0, backbone_path=None,
                  pretrained=False):
         super(CspDarkNet, self).__init__()
         self.subtype = subtype
@@ -32,26 +28,10 @@ class CspDarkNet(nn.Module):
         self.backbone_path = backbone_path
         self.pretrained = pretrained
 
-        '''
-        if self.subtype == 'cspdark_s':
-            dep_mul, wid_mul = 0.33, 0.5
-            self.out_channels = [32, 64, 128, 256, 512]
-        elif self.subtype == 'cspdark_m':
-            dep_mul, wid_mul = 0.67, 0.75
-            self.out_channels = [48, 96, 192, 384, 768]
-        elif self.subtype == 'cspdark_l':
-            dep_mul, wid_mul = 1.0, 1.0
-            self.out_channels = [64, 128, 256, 512, 1024]
-        elif self.subtype == 'cspdark_x':
-            dep_mul, wid_mul = 1.33, 1.25
-            self.out_channels = [80, 160, 320, 640, 1280]
-        else:
-            raise NotImplementedError
-        '''
-        depth_mul, width_mul = self.cfg[self.subtype.split("_")[1]]
-        self.out_channels = [64, 128, 256, 512, 1024]
+        # depth_mul, width_mul = self.cfg[self.subtype.split("_")[1]]
+        out_channels = [64, 128, 256, 512, 1024]
         layers = [3, 9, 9, 3]
-        self.out_channels = in_places = list(map(lambda x: int(x * width_mul), self.out_channels))
+        self.out_channels = in_places = list(map(lambda x: int(x * width_mul), out_channels))
         layers = list(map(lambda x: max(round(x * depth_mul), 1), layers))
 
         act = "silu"
