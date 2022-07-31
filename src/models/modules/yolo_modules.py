de3c07964a42236db69923862f1c7b45fd4912ea
liumin <569793357@qq.com>
2022-07-31 21:16:21 +0800

    rename

--- src/models/modules/yolo_modules.py
+++ src/models/modules/yolo_modules.py
@@ -2,13 +2,14 @@
 # -- coding: utf-8 --
 # @Time : 2021/1/8 16:51
 # @Author : liumin
-# @File : yolov5_modules.py
+# @File : yolo_modules.py
 
 import math
 import warnings
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
 def autopad(k, p=None):  # kernel, padding
@@ -356,12 +357,12 @@ class Expand(nn.Module):
 
 class Concat(nn.Module):
     # Concatenate a list of tensors along dimension
-    def __init__(self, dimension=1):
+    def __init__(self, dim=1):
         super(Concat, self).__init__()
-        self.d = dimension
+        self.dim = dim
 
     def forward(self, x):
-        return torch.cat(x, self.d)
+        return torch.cat(x, self.dim)
 
 
 class Flatten(nn.Module):
@@ -369,64 +370,6 @@ class Flatten(nn.Module):
     @staticmethod
     def forward(x):
         return x.view(x.size(0), -1)
-
-
-class Detect(nn.Module):
-    stride = None  # strides computed during build
-    def __init__(self, num_classes=80, stride=[8., 16., 32.], anchors=(), ch=()):  # detection layer
-        super().__init__()
-        self.num_classes = num_classes  # number of classes
-        self.num_outputs = num_classes + 5  # number of outputs per anchor
-        self.num_layers = len(anchors)  # number of detection layers
-        self.num_anchors = len(anchors[0])  # number of anchors
-        self.stride = stride
-        self.grid = [torch.zeros(1)] * self.num_layers  # init grid
-        self.anchor_grid = [torch.zeros(1)] * self.num_layers  # init anchor grid
-        self.register_buffer('anchors', torch.tensor(anchors).float())  # shape(num_layers,num_anchors,2)
-        self.m = nn.ModuleList(nn.Conv2d(x, self.num_outputs * self.num_anchors, 1) for x in ch)  # output conv
-
-        self._init_weight()
-
-    def _init_weight(self, cf=None):
-        # https://arxiv.org/abs/1708.02002 section 3.3
-        # cf = torch.bincount(torch.tensor(np.concatenate(dataset.labels, 0)[:, 0]).long(), minlength=num_classes) + 1.
-        for mi, s in zip(self.m, self.stride):  # from
-            b = mi.bias.view(self.num_anchors, -1)  # conv.bias(255) to (3,85)
-            b.data[:, 4] += math.log(8 / (640 / s) ** 2)  # obj (8 objects per 640 image)
-            b.data[:, 5:] += math.log(0.6 / (self.num_classes - 0.999999)) if cf is None else torch.log(cf / cf.sum())  # cls
-            mi.bias = torch.nn.Parameter(b.view(-1), requires_grad=True)
-
-
-    def forward(self, x):
-        z = []  # inference output
-        for i in range(self.num_layers):
-            x[i] = self.m[i](x[i])  # conv
-            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-            x[i] = x[i].view(bs, self.num_anchors, self.num_outputs, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
-
-            if not self.training:  # inference
-                if self.grid[i].shape[2:4] != x[i].shape[2:4]:
-                    self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
-
-                y = x[i].sigmoid()
-
-                y[..., 0:2] = (y[..., 0:2] * 2 - 0.5 + self.grid[i]) * self.stride[i]  # xy
-                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-
-                z.append(y.view(bs, -1, self.num_outputs))
-
-        # return x if self.training else (torch.cat(z, 1), x)
-        # return torch.cat(z, 1), x
-        return (None, x) if self.training else (torch.cat(z, 1), None)
-
-
-    def _make_grid(self, nx=20, ny=20, i=0):
-        d = self.anchors[i].device
-        yv, xv = torch.meshgrid([torch.arange(ny).to(d), torch.arange(nx).to(d)])
-        grid = torch.stack((xv, yv), 2).expand((1, self.num_anchors, ny, nx, 2)).float()
-        anchor_grid = (self.anchors[i].clone() * self.stride[i]) \
-            .view((1, self.num_anchors, 1, 1, 2)).expand((1, self.num_anchors, ny, nx, 2)).float()
-        return grid, anchor_grid
 
 
 class Classify(nn.Module):
@@ -442,37 +385,23 @@ class Classify(nn.Module):
         return self.flat(self.conv(z))  # flatten to x(b,c2)
 
 
+class UpsamplingModule(nn.Module):
+    def __init__(self, c1, c2, layer=3):
+        super(UpsamplingModule, self).__init__()
+        self.conv = Conv(c1, c2, 1, 1)
+        self.up = nn.UpsamplingNearest2d(scale_factor=2)
+        self.fuse = C3(c2 * 2, c2, layer, False)
 
-def parse_yolov5_model(cfg, depth_mul=1, width_mul=1):
-    layers = []
-    ch_list = [3]
-    ch_out = ch_list[-1]
-    # number of outputs = anchors * (classes + 5)
-    for i, (f, n, m, args) in enumerate(cfg):
-        m = eval(m) if isinstance(m, str) else m  # eval strings
-        for j, a in enumerate(args):
-            args[j] = eval(a) if isinstance(a, str) else a  # eval strings
+    def forward(self, x, y):
+        x_conv = self.conv(x)
+        return self.fuse(torch.cat([self.up(x_conv), y], dim=1)), x_conv
 
-        n = max(round(n * depth_mul), 1) if n > 1 else n  # depth gain
-        if m in [Conv, Bottleneck, SPP, DWConv, MixConv2d, Focus, CrossConv, BottleneckCSP, C3]:
-            ch_in, ch_out = ch_list[f], args[0]
-            ch_out = make_divisible(ch_out * width_mul, 8)
-            args = [ch_in, ch_out, *args[1:]]
-            if m in [BottleneckCSP, C3]:
-                args.insert(2, n)
-                n = 1
-        elif m is nn.BatchNorm2d:
-            args = [ch_list[f]]
-        elif m is Concat:
-            ch_out = sum([ch_list[-1 if x == -1 else x + 1] for x in f])
-        elif m is Contract:
-            ch_out = ch_list[f] * args[0] ** 2
-        elif m is Expand:
-            ch_out = ch_list[f] // args[0] ** 2
-        else:
-            ch_out = ch_list[f]
 
-        m_ = nn.Sequential(*[m(*args) for _ in range(n)]) if n > 1 else m(*args)  # module
-        layers.append(m_)
-        ch_list.append(ch_out)
-    return nn.Sequential(*layers)
+class DownsamplingModule(nn.Module):
+    def __init__(self, c1, c2, layer=3):
+        super(DownsamplingModule, self).__init__()
+        self.down = Conv(c1, c1, 3, 2)
+        self.fuse = C3(c1 * 2, c2, layer, False)
+
+    def forward(self, x, y):
+        return self.fuse(torch.cat([self.down(x), y], dim=1))
