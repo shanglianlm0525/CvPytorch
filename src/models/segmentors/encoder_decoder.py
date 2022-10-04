7ce604eea0a073993abcc3b890337956c80dbf38
liumin <569793357@qq.com>
2022-10-04 15:05:40 +0800

    update

--- src/models/segmentors/encoder_decoder.py
+++ src/models/segmentors/encoder_decoder.py
@@ -9,12 +9,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
 
+from src.utils.config import CommonConfiguration
+from src.models.segmentors.base_segmentor import BaseSegmentor
 from src.models.backbones import build_backbone
 from src.models.heads import build_head
 from src.models.necks import build_neck
 from src.losses import build_loss
-from src.models.segmentors.base_segmentor import BaseSegmentor
-from src.utils.config import CommonConfiguration
+from src.utils.misc import add_prefix
 
 
 class EncoderDecoder(BaseSegmentor):
@@ -40,8 +41,11 @@ class EncoderDecoder(BaseSegmentor):
         self.head = build_head(self.model_cfg.HEAD)
         # AUX_HEAD
         if self.model_cfg.AUX_HEAD is not None:
+            assert self.model_cfg.AUX_LOSS is not None, f'{self.model_cfg.AUX_LOSS} must not be None'
+            assert type(self.model_cfg.AUX_HEAD) == type(self.model_cfg.AUX_LOSS)
+            assert isinstance(self.model_cfg.AUX_HEAD, (list, tuple)) and len(self.model_cfg.AUX_HEAD) == len(self.model_cfg.AUX_LOSS)
             if isinstance(self.model_cfg.AUX_HEAD, (dict, CommonConfiguration)):
-                self.auxiliary_head = build_head(self.model_cfg.AUX_HEAD)
+                self.auxiliary_head = [build_head(self.model_cfg.AUX_HEAD)]
             elif isinstance(self.model_cfg.AUX_HEAD, (list, tuple)):
                 self.auxiliary_head = nn.ModuleList()
                 for head_cfg in self.model_cfg.AUX_HEAD:
@@ -60,6 +64,18 @@ class EncoderDecoder(BaseSegmentor):
             raise TypeError(f'self.model_cfg.LOSS must be a dict or sequence of dict,\
                        but got {type(self.model_cfg.LOSS)}')
 
+        # AUX_LOSS
+        if self.model_cfg.AUX_LOSS is not None:
+            if isinstance(self.model_cfg.AUX_LOSS, (dict, CommonConfiguration)):
+                self.auxiliary_loss = [build_loss(self.model_cfg.AUX_LOSS)]
+            elif isinstance(self.model_cfg.AUX_LOSS, (list, tuple)):
+                self.auxiliary_loss = nn.ModuleList()
+                for loss_cfg in self.model_cfg.AUX_LOSS:
+                    self.auxiliary_loss.append(build_loss(loss_cfg))
+            else:
+                raise TypeError(f'self.model_cfg.AUX_LOSS must be a dict or sequence of dict,\
+                                       but got {type(self.model_cfg.AUX_LOSS)}')
+
     def setup_extra_params(self):
         pass
         '''
@@ -70,30 +86,22 @@ class EncoderDecoder(BaseSegmentor):
                     head_cfg.__setitem__('num_classes', self.num_classes)
             else:
                 self.model_cfg.AUX_HEAD.__setitem__('num_classes', self.num_classes)
-        
-        if self.model_cfg.LOSS is not None:
-            if isinstance(self.model_cfg.LOSS, list):
-                for head_cfg in self.model_cfg.LOSS:
-                    head_cfg.__setitem__('num_classes', self.num_classes)
-            else:
-                self.model_cfg.LOSS.__setitem__('num_classes', self.num_classes)
         '''
 
-    def loss_forward(self, preds, targets):
+    def loss_forward(self, preds, targets, loss):
         assert isinstance(preds, Tensor) and preds.ndim == 4, \
             f'preds must be Tensor type, and ndim == 4, but got {type(preds)} and {preds.ndim}'
         assert isinstance(targets, Tensor) and targets.ndim == 3, \
             f'targets must be Tensor type, and ndim == 3, but got {type(targets)} and {targets.ndim}'
         losses = dict()
         preds = F.interpolate(preds, size=targets.shape[-2:], mode='bilinear', align_corners=False)
-        for l in self.loss:
+        for l in loss:
             if l.loss_name not in losses:
                 losses[l.loss_name] = l(preds, targets)
             else:
                 losses[l.loss_name] += l(preds, targets)
         losses['loss'] = sum(losses.values())
         return losses
-
 
     def forward(self, imgs, targets=None, mode='infer', **kwargs):
         """
@@ -103,26 +111,25 @@ class EncoderDecoder(BaseSegmentor):
             targets: (Tensor | list[Tensor])
                 Shape: N x H x W.
             mode: (str) run type of forward.
-                Options: 'resize_concat', 'multiple_select', None.
+                Options: 'train', 'val', 'infer'.
                 Default: 'infer'.
         """
         feats = self.backbone(imgs)
         if self.with_neck:
             feats = self.neck(feats)
         preds = self.head(feats)
-        if self.with_auxiliary_head:
-            preds = [preds]
-            if isinstance(self.auxiliary_head, nn.ModuleList):
-                for aux_head in self.auxiliary_head:
-                    preds.append(aux_head(feats))
-            else:
-                preds.append(self.auxiliary_head(feats))
 
         if mode == 'infer':
 
             return torch.argmax(feats, dim=1)
         else:
-            losses = self.loss_forward(preds, targets)
+            losses = self.loss_forward(preds, targets, self.loss)
+
+            if self.with_auxiliary_head:
+                for i, (aux_head, auxiliary_loss) in enumerate(zip(self.auxiliary_head, self.auxiliary_loss)):
+                    aux_pred = aux_head(feats)
+                    aux_losses = self.loss_forward(aux_pred, targets, auxiliary_loss)
+                    losses.update(add_prefix(aux_losses, 'aux'+str(i)))
 
             if mode == 'val':
                 return losses, torch.argmax(preds, dim=1)
