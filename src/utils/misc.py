b37b91019086539daafbd3bb9f2e14a85dcd992a
liumin <569793357@qq.com>
2022-10-05 09:58:08 +0800

    add add_prefix function

---
+++ src/utils/misc.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2022/10/4 10:58
+# @Author : liumin
+# @File : misc.py
+
+def add_prefix(inputs, prefix):
+    """Add prefix for dict.
+
+    Args:
+        inputs (dict): The input dict with str keys.
+        prefix (str): The prefix to add.
+
+    Returns:
+
+        dict: The dict with keys updated with ``prefix``.
+    """
+
+    outputs = dict()
+    for name, value in inputs.items():
+        outputs[f'{prefix}.{name}'] = value
+
+    return outputs
