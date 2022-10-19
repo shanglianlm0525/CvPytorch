bf239cf6a3384fd6b7a97521abffc28119bbd275
liumin <569793357@qq.com>
2022-10-19 21:21:12 +0800

    add MixTransformer

--- src/models/backbones/seg/__init__.py
+++ src/models/backbones/seg/__init__.py
@@ -3,3 +3,10 @@
 # @Time : 2022/9/26 19:47
 # @Author : liumin
 # @File : __init__.py.py
+
+from .mscan import MSCAN
+from .stdcnet import STDCNet
+from .topformer_backbone import TopFormerBackbone
+from .regseg_backbone import RegSegBackbone
+from .resnet import ResNet
+from .mix_transformer import MixVisionTransformer
