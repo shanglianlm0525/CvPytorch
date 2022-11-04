831bab4217c0a85049993fa2687d9787cdc2c129
liumin <569793357@qq.com>
2022-11-04 08:59:03 +0800

    add make_divisible and make_round

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -3,6 +3,8 @@
 # @Time : 2022/10/4 10:58
 # @Author : liumin
 # @File : misc.py
+import math
+
 
 def add_prefix(inputs, prefix):
     """Add prefix for dict.
@@ -21,3 +23,15 @@ def add_prefix(inputs, prefix):
         outputs[f'{prefix}_{name}'] = value
 
     return outputs
+
+
+def make_divisible(x: float,
+                   widen_factor: float = 1.0,
+                   divisor: int = 8) -> int:
+    """Make sure that x*widen_factor is divisible by divisor."""
+    return math.ceil(x * widen_factor / divisor) * divisor
+
+
+def make_round(x: float, deepen_factor: float = 1.0) -> int:
+    """Make sure that x*deepen_factor becomes an integer not less than 1."""
+    return max(round(x * deepen_factor), 1) if x > 1 else x
