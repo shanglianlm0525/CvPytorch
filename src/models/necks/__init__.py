276bb30f947f14ac270758090b952e16e6c6e23c
liumin <569793357@qq.com>
2022-11-16 08:44:40 +0800

    update 20221116

--- src/models/necks/__init__.py
+++ src/models/necks/__init__.py
@@ -14,15 +14,25 @@ from .ghost_pan import GhostPAN
 from .giraffeneck import GiraffeNeck
 from .lfd_neck import LFDNeck
 from .pan import PAN
-from .seg.stdc_neck import STDCNeck
+
 from .tan import TAN
 from .yolop_neck import YOLOPNeck
 from .yolov3_neck import YOLOv3Neck
 from .yolov5_neck import YOLOv5Neck
 from .yolov6_neck import YOLOv6Neck
 from .yolov7_neck import YOLOv7Neck
-from .yolox_neck import YOLOXNeck
-from .pai_yolox_neck import PAI_YOLOXNeck
+# from .pai_yolox_neck import PAI_YOLOXNeck
+
+
+# Image Classification
+
+
+# Semantic Segmentation
+from .seg import PSPNeck
+from .seg import STDCNeck
+
+# Object Detectiton
+from .det import YOLOXNeck
 
 __all__ = [
     'FPN',
@@ -30,7 +40,7 @@ __all__ = [
     'TAN',
     'BiFPN',
     'YOLOXNeck',
-    'PAI_YOLOXNeck',
+    # 'PAI_YOLOXNeck',
     'YOLOv3Neck',
     'YOLOv5Neck',
     'YOLOv6Neck',
@@ -42,6 +52,7 @@ __all__ = [
     'FastestDetNeck',
     'GiraffeNeck',
     'STDCNeck',
+    'PSPNeck',
 ]
 
 def build_neck(cfg):
@@ -57,8 +68,8 @@ def build_neck(cfg):
         return BiFPN(**neck_cfg)
     elif name == 'YOLOXNeck':
         return YOLOXNeck(**neck_cfg)
-    elif name == 'PAI_YOLOXNeck':
-        return PAI_YOLOXNeck(**neck_cfg)
+    # elif name == 'PAI_YOLOXNeck':
+    #    return PAI_YOLOXNeck(**neck_cfg)
     elif name == 'YOLOv3Neck':
         return YOLOv3Neck(**neck_cfg)
     elif name == 'YOLOv5Neck':
@@ -81,5 +92,7 @@ def build_neck(cfg):
         return LFDNeck(**neck_cfg)
     elif name == 'STDCNeck':
         return STDCNeck(**neck_cfg)
+    elif name == 'PSPNeck':
+        return PSPNeck(**neck_cfg)
     else:
         raise NotImplementedError
