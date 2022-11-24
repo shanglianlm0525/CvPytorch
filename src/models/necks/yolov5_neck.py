2ac633bcfaa88382478e576806616115f08ee81e
liumin <569793357@qq.com>
2022-11-24 08:43:22 +0800

    update 20221124

--- src/models/necks/yolov5_neck.py
+++ src/models/necks/yolov5_neck.py
@@ -6,7 +6,7 @@
 
 import torch
 import torch.nn as nn
-from src.models.modules.yolo_modules import UpsamplingModule, DownsamplingModule
+from src.models.modules.yolo11_modules import UpsamplingModule, DownsamplingModule
 
 
 class YOLOv5Neck(nn.Module):
