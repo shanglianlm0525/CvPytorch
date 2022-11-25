55a97fa86cb750e75ade741395786a3df0e12b17
liumin <569793357@qq.com>
2022-11-25 08:56:56 +0800

    update 20221125

--- src/models/modules/yolox_modules.py
+++ src/models/modules/yolox_modules.py
@@ -3,9 +3,14 @@
 # @Time : 2021/7/26 14:23
 # @Author : liumin
 # @File : yolox_modules.py
+import warnings
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from src.models.modules.convs import ConvModule
 
 
 class SiLU(nn.Module):
@@ -128,7 +133,8 @@ class CSPLayer(nn.Module):
 class Focus(nn.Module):
     """Focus width and height information into channel space."""
 
-    def __init__(self, in_channels, out_channels, ksize=1, stride=1, act="silu"):
+    def __init__(self, in_channels, out_channels, ksize=1, stride=1,
+                 conv_cfg=None, norm_cfg=dict(type='BN', momentum=0.03, eps=0.001), act_cfg=dict(type='Swish')):
         super().__init__()
         self.conv = BaseConv(in_channels * 4, out_channels, ksize, stride, act=act)
 
@@ -161,3 +167,201 @@ class SPPBottleneck(nn.Module):
         x = torch.cat([x] + [m(x) for m in self.m], dim=1)
         x = self.conv2(x)
         return x
+
+
+### PAI-YOLOX
+
+class RepVGGBlock(nn.Module):
+    """
+        Basic Block of RepVGG
+        It's an efficient block that will be reparameterized in evaluation. (deploy = True)
+        Usage: RepVGGBlock(in_channels, out_channels, ksize=3, stride=stride)
+
+    """
+
+    def __init__(self, in_channels, out_channels,
+                 ksize=3, stride=1, padding=1, dilation=1, groups=1,
+                 padding_mode='zeros', deploy=False, act=None):
+        super(RepVGGBlock, self).__init__()
+        self.deploy = deploy
+        self.groups = groups
+        self.in_channels = in_channels
+
+        assert ksize == 3
+        assert padding == 1
+
+        padding_11 = padding - ksize // 2
+
+        self.nonlinearity = nn.ReLU()
+        self.se = nn.Identity()
+
+        if deploy:
+            self.rbr_reparam = nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=ksize,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+                bias=True,
+                padding_mode=padding_mode)
+
+        else:
+            self.rbr_identity = nn.BatchNorm2d(
+                num_features=in_channels
+            ) if out_channels == in_channels and stride == 1 else None
+            self.rbr_dense = nn.Sequential(
+                nn.Conv2d(in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=ksize,
+                    stride=stride,
+                    padding=padding,
+                    groups=groups,
+                    bias=False),
+                nn.BatchNorm2d(num_features=out_channels)
+            )
+            self.rbr_1x1 = nn.Sequential(
+                nn.Conv2d(in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=1,
+                    stride=stride,
+                    padding=padding_11,
+                    groups=groups,
+                    bias=False),
+                nn.BatchNorm2d(num_features=out_channels)
+            )
+
+    def forward(self, inputs):
+        if hasattr(self, 'rbr_reparam'):
+            return self.nonlinearity(self.se(self.rbr_reparam(inputs)))
+
+        if self.rbr_identity is None:
+            id_out = 0
+        else:
+            id_out = self.rbr_identity(inputs)
+
+        return self.nonlinearity(
+            self.se(self.rbr_dense(inputs) + self.rbr_1x1(inputs) + id_out))
+
+    #   Optional. This improves the accuracy and facilitates quantization.
+    #   1.  Cancel the original weight decay on rbr_dense.conv.weight and rbr_1x1.conv.weight.
+    #   2.  Use like this.
+    #       loss = criterion(....)
+    #       for every RepVGGBlock blk:
+    #           loss += weight_decay_coefficient * 0.5 * blk.get_cust_L2()
+    #       optimizer.zero_grad(set_to_none=True)
+    #       loss.backward()
+
+    def get_custom_L2(self):
+        K3 = self.rbr_dense.conv.weight
+        K1 = self.rbr_1x1.conv.weight
+        t3 = (self.rbr_dense.bn.weight /
+              ((self.rbr_dense.bn.running_var +
+                self.rbr_dense.bn.eps).sqrt())).reshape(-1, 1, 1, 1).detach()
+        t1 = (self.rbr_1x1.bn.weight / ((self.rbr_1x1.bn.running_var +
+                                         self.rbr_1x1.bn.eps).sqrt())).reshape(
+                                             -1, 1, 1, 1).detach()
+
+        l2_loss_circle = (K3**2).sum() - (K3[:, :, 1:2, 1:2]**2).sum(
+        )  # The L2 loss of the "circle" of weights in 3x3 kernel. Use regular L2 on them.
+        eq_kernel = K3[:, :, 1:2, 1:
+                       2] * t3 + K1 * t1  # The equivalent resultant central point of 3x3 kernel.
+        l2_loss_eq_kernel = (eq_kernel**2 / (t3**2 + t1**2)).sum(
+        )  # Normalize for an L2 coefficient comparable to regular L2.
+        return l2_loss_eq_kernel + l2_loss_circle
+
+    #   This func derives the equivalent kernel and bias in a DIFFERENTIABLE way.
+    #   You can get the equivalent kernel and bias at any time and do whatever you want,
+    #   for example, apply some penalties or constraints during training, just like you do to the other models.
+    #   May be useful for quantization or pruning.
+    def get_equivalent_kernel_bias(self):
+        kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
+        kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
+        kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
+        return kernel3x3 + self._pad_1x1_to_3x3_tensor(
+            kernel1x1) + kernelid, bias3x3 + bias1x1 + biasid
+
+    def _pad_1x1_to_3x3_tensor(self, kernel1x1):
+        if kernel1x1 is None:
+            return 0
+        else:
+            return F.pad(kernel1x1, [1, 1, 1, 1])
+
+    def _fuse_bn_tensor(self, branch):
+        if branch is None:
+            return 0, 0
+        if isinstance(branch, nn.Sequential):
+            kernel = branch.conv.weight
+            running_mean = branch.bn.running_mean
+            running_var = branch.bn.running_var
+            gamma = branch.bn.weight
+            beta = branch.bn.bias
+            eps = branch.bn.eps
+        else:
+            assert isinstance(branch, nn.BatchNorm2d)
+            if not hasattr(self, 'id_tensor'):
+                input_dim = self.in_channels // self.groups
+                kernel_value = np.zeros((self.in_channels, input_dim, 3, 3),
+                                        dtype=np.float32)
+                for i in range(self.in_channels):
+                    kernel_value[i, i % input_dim, 1, 1] = 1
+                self.id_tensor = torch.from_numpy(kernel_value).to(
+                    branch.weight.device)
+            kernel = self.id_tensor
+            running_mean = branch.running_mean
+            running_var = branch.running_var
+            gamma = branch.weight
+            beta = branch.bias
+            eps = branch.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta - running_mean * gamma / std
+
+    def switch_to_deploy(self):
+        if hasattr(self, 'rbr_reparam'):
+            return
+        kernel, bias = self.get_equivalent_kernel_bias()
+        self.rbr_reparam = nn.Conv2d(
+            in_channels=self.rbr_dense.conv.in_channels,
+            out_channels=self.rbr_dense.conv.out_channels,
+            kernel_size=self.rbr_dense.conv.kernel_size,
+            stride=self.rbr_dense.conv.stride,
+            padding=self.rbr_dense.conv.padding,
+            dilation=self.rbr_dense.conv.dilation,
+            groups=self.rbr_dense.conv.groups,
+            bias=True)
+        self.rbr_reparam.weight.data = kernel
+        self.rbr_reparam.bias.data = bias
+        for para in self.parameters():
+            para.detach_()
+        self.__delattr__('rbr_dense')
+        self.__delattr__('rbr_1x1')
+        if hasattr(self, 'rbr_identity'):
+            self.__delattr__('rbr_identity')
+        if hasattr(self, 'id_tensor'):
+            self.__delattr__('id_tensor')
+        self.deploy = True
+
+
+
+class MT_SPPF(nn.Module):
+    '''Simplified SPPF with ReLU activation'''
+
+    def __init__(self, in_channels, out_channels, kernel_size=5):
+        super().__init__()
+        c_ = in_channels // 2  # hidden channels
+        # self.cv1 = ConvBNReLU(in_channels, c_, 1, 1)
+        # self.cv2 = ConvBNReLU(c_ * 4, out_channels, 1, 1)
+        self.cv1 = ConvModule(in_channels, c_, 1, 1)
+        self.cv2 = ConvModule(c_ * 4, out_channels, 1, 1)
+        self.maxpool = nn.MaxPool2d(
+            kernel_size=kernel_size, stride=1, padding=kernel_size // 2)
+
+    def forward(self, x):
+        x = self.cv1(x)
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            y1 = self.maxpool(x)
+            y2 = self.maxpool(y1)
+            return self.cv2(torch.cat([x, y1, y2, self.maxpool(y2)], 1))
