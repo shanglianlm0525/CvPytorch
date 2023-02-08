ab6115c80567679fc28517f7f8617a1c72d567f0
liumin <569793357@qq.com>
2023-02-08 09:01:29 +0800

    update yolov6 backbone 3.0

--- src/models/backbones/det/yolov6_efficient_rep.py
+++ src/models/backbones/det/yolov6_efficient_rep.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python
 # -- coding: utf-8 --
-# @Time : 2022/10/28 8:50
+# @Time : 2023/2/1 10:37
 # @Author : liumin
-# @File : yolov6_efficient_rep.py
+# @File : yolov6_efficient_rep.py.py
 
 import torch
 import torch.nn as nn
@@ -10,8 +10,12 @@ import math
 from torch.nn.modules.batchnorm import _BatchNorm
 
 from src.models.backbones.det.base_yolo_backbone import BaseYOLOBackbone
-from src.models.modules.yolo_modules import SPPF, CSPStackRep, RepBlock, RepVGGBlock
+from src.models.modules.yolo_modules import RepVGGBlock, SimCSPSPPF, SimSPPF, ConvWrapper, BepC3, RepBlock
 
+"""
+    YOLOv6 v3.0: A Full-Scale Reloading
+    https://arxiv.org/pdf/2301.05586.pdf
+"""
 
 class YOLOv6EfficientRep(BaseYOLOBackbone):
     cfg = {"n": [0.33, 0.25],
@@ -21,23 +25,23 @@ class YOLOv6EfficientRep(BaseYOLOBackbone):
             "l": [1.0, 1.0],
             "x": [1.33, 1.25]}
     csp_e_cfg = { "n": None, "t": None, "s": None, "m": float(2)/3, "l": float(1)/2, "x": float(1)/2 }
-    def __init__(self, subtype='cspdark_s', in_channels=3, out_channels=[64, 128, 256, 512, 1024], num_blocks=[6, 12, 18, 6], spp_ksizes=5, depthwise=False,
+    def __init__(self, subtype='cspdark_s', in_channels=3, out_channels=[64, 128, 256, 512, 1024], num_blocks=[6, 12, 18, 6], depthwise=False,
                  conv_cfg=None, norm_cfg=dict(type='BN', requires_grad=True), act_cfg=dict(type='SiLU', inplace=True),
                  out_stages=[2, 3, 4], output_stride=32, backbone_path=None, pretrained=False,
-                 frozen_stages=-1, norm_eval=False):
+                 frozen_stages=-1, norm_eval=False,**kwargs):
         stype = subtype.split("_")[1]
-        self.spp_ksizes = spp_ksizes
-        self.block = CSPStackRep if stype in ['m', 'l', 'x'] else RepBlock
+        self.basic_block= ConvWrapper if stype in ['l', 'x'] else RepVGGBlock
+        self.block = BepC3 if stype in ['m', 'l', 'x'] else RepBlock
         self.csp_e = self.csp_e_cfg[stype]
+        self.use_cspsppf = True if stype in ['n', 't', 's'] else False
         super(YOLOv6EfficientRep, self).__init__(subtype=subtype, cfg=self.cfg, in_channels=in_channels, out_channels=out_channels, num_blocks=num_blocks, depthwise=depthwise,
                                                conv_cfg=conv_cfg, norm_cfg=norm_cfg, act_cfg=act_cfg, output_stride=output_stride,
                                                out_stages = out_stages, pretrained = pretrained, backbone_path = backbone_path,
-                                               frozen_stages = frozen_stages,norm_eval = norm_eval)
-
+                                               frozen_stages = frozen_stages,norm_eval = norm_eval, **kwargs)
 
     def build_stem_layer(self):
         """Build a stem layer."""
-        return RepVGGBlock(in_channels=self.in_channels, out_channels=self.out_channels[0], kernel_size=3, stride=2)
+        return self.basic_block(in_channels=self.in_channels, out_channels=self.out_channels[0], kernel_size=3, stride=2)
 
 
     def build_stage_layer(self, idx, in_channels, out_channels, num_blocks):
@@ -48,10 +52,13 @@ class YOLOv6EfficientRep(BaseYOLOBackbone):
             setting (list): The architecture setting of a stage layer.
         """
         stage = []
-        stage.append(RepVGGBlock(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=2))
-        stage.append(self.block(in_channels=out_channels, out_channels=out_channels, n=num_blocks, e=self.csp_e))
+        stage.append(self.basic_block(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=2))
+        stage.append(self.block(in_channels=out_channels, out_channels=out_channels, n=num_blocks, e=self.csp_e, block=self.basic_block))
         if idx == 3:
-            spp = SPPF(in_channels=out_channels, out_channels=out_channels, kernel_sizes=self.spp_ksizes, act_cfg=dict(type='ReLU'))
+            if self.use_cspsppf:
+                spp = SimCSPSPPF(in_channels=out_channels, out_channels=out_channels, act_cfg=dict(type='ReLU'))
+            else:
+                spp = SimSPPF(in_channels=out_channels, out_channels=out_channels, act_cfg=dict(type='Swish'))
             stage.append(spp)
         return stage
 
